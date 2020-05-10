1b606806b25b8e1d970804a660ea40b218197081
Herr Zatacke <zatacke@gmail.com>
2020-05-10 22:11:11 +0200

    basic navigation styling

--- src/javascript/app/components/App/component.jsx
+++ src/javascript/app/components/App/component.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import Navigation from '../Navigation';
 import GameBoyImage from '../GameBoyImage';
 
 const App = (/* props */) => (
-  <>
+  <Router>
     <Navigation />
     <GameBoyImage />
-  </>
+    <Switch>
+      <Route path="/gallery">
+        <div>Gallery</div>
+      </Route>
+      <Route path="/palettes">
+        <div>Palettes</div>
+      </Route>
+      <Route path="/settings">
+        <div>Settings</div>
+      </Route>
+      <Route path="/">
+        <div>Home</div>
+      </Route>
+    </Switch>
+  </Router>
 );
 
 App.propTypes = {
--- src/javascript/app/components/Navigation/component.jsx
+++ src/javascript/app/components/Navigation/component.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const Navigation = (/* props */) => (
   <nav className="navigation">
-    <ul>
-      <li>
-        Home
+    <ul className="navigation__list">
+      <li className="navigation__entry">
+        <Link to="/" className="navigation__link">
+          Home
+        </Link>
       </li>
-      <li>
-        Gallery
+      <li className="navigation__entry">
+        <Link to="/gallery" className="navigation__link">
+          Gallery
+        </Link>
       </li>
-      <li>
-        Palettes
+      <li className="navigation__entry">
+        <Link to="/palettes" className="navigation__link">
+          Palettes
+        </Link>
+      </li>
+      <li className="navigation__entry">
+        <Link to="/settings" className="navigation__link">
+          Settings
+        </Link>
       </li>
     </ul>
   </nav>
