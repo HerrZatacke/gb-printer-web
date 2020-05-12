93c33bea2db655cb407d2683b6babec7f5a5eb2d
Herr Zatacke <zatacke@gmail.com>
2020-05-12 19:47:12 +0200

    Do not autoconnect to ws-server when on prod

--- src/javascript/app/store/middlewares/serialportWebocket.js
+++ src/javascript/app/store/middlewares/serialportWebocket.js
@@ -3,35 +3,47 @@ import handleLines from '../../../tools/handleLines';
 
 const WEBSOCKETS_PROTOCOL = window.location.protocol === 'https:' ? 'wss:' : 'ws';
 
+const newSocket = (dispatch, socketUrl) => (
+  new Sockette(socketUrl, {
+    timeout: 5000,
+    maxAttempts: 10,
+    onstatechange: (readyState) => {
+      dispatch({
+        type: 'SET_SOCKET_STATE',
+        payload: readyState,
+      });
+    },
+    onmessage: ({ data }) => {
+      data
+        .split('\n')
+        .map(handleLines)
+        .filter(Boolean)
+        .forEach(dispatch);
+    },
+  })
+);
+
 const serialportWebocket = (store) => {
 
   const socketUrl = store.getState().socketUrl;
 
   let socket;
 
-  window.setTimeout(() => {
-    socket = new Sockette(`${WEBSOCKETS_PROTOCOL}//${socketUrl}`, {
-      timeout: 5000,
-      maxAttempts: 10,
-      onstatechange: (readyState) => {
-        store.dispatch({
-          type: 'SET_SOCKET_STATE',
-          payload: readyState,
-        });
-      },
-      onmessage: ({ data }) => {
-        data
-          .split('\n')
-          .map(handleLines)
-          .filter(Boolean)
-          .forEach(store.dispatch);
-      },
-    });
-  }, 1000);
+  // Do not autoconnect when on prod
+  if (socketUrl && ENV !== 'production') {
+    window.setTimeout(() => {
+      socket = newSocket(store.dispatch, `${WEBSOCKETS_PROTOCOL}//${socketUrl}`);
+    }, 1000);
+  }
 
   return (next) => (action) => {
     if (action.type === 'SET_SOCKET_URL') {
-      socket.setUrl(`${WEBSOCKETS_PROTOCOL}//${action.payload}`);
+      const protocolSocketUrl = `${WEBSOCKETS_PROTOCOL}//${action.payload}`;
+      if (socket) {
+        socket.setUrl(protocolSocketUrl);
+      } else {
+        socket = newSocket(store.dispatch(), protocolSocketUrl);
+      }
     }
 
     return next(action);
