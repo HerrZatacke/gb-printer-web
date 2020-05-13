c6c60b505f35a9ea1b3047538aeed6b827942892
Herr Zatacke <zatacke@gmail.com>
2020-05-13 02:20:47 +0200

    add dummy example dump

--- src/javascript/app/components/Dump/component.jsx
+++ src/javascript/app/components/Dump/component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import dummyContent from './dummy';
 
 class Dump extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      text: dummyContent.join('\n'),
     };
   }
 
