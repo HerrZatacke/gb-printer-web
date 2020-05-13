f2be7cbe34658d1fee00d3e87e3844265ff1321b
Herr Zatacke <zatacke@gmail.com>
2020-05-13 02:01:21 +0200

    add todos on startpage

--- src/javascript/app/components/Home/component.jsx
+++ src/javascript/app/components/Home/component.jsx
@@ -26,6 +26,27 @@ const Home = () => (
     <a href="https://github.com/HerrZatacke/gb-printer-web">
       This project on GitHub
     </a>
+    <h3>ToDo:</h3>
+    <ul>
+      <li>
+        Edit images
+      </li>
+      <li>
+        Edit palettes
+      </li>
+      <li>
+        Export data/single images
+      </li>
+      <li>
+        Save images in different resolutions / imagetypes
+      </li>
+      <li>
+        Allow rendering animated Gifs
+      </li>
+      <li>
+        Allow rendering rgb images with/without 4th contrast image
+      </li>
+    </ul>
   </div>
 );
 
