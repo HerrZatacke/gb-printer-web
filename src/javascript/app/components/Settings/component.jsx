9332a88aa73f393c339115e8d5df14c2fce479cb
Herr Zatacke <zatacke@gmail.com>
2020-05-15 02:48:15 +0200

    change available download resolutions

--- src/javascript/app/components/Settings/component.jsx
+++ src/javascript/app/components/Settings/component.jsx
@@ -27,7 +27,7 @@ class Settings extends React.Component {
           <div className="settings__label">
             Image export dimensions
           </div>
-          {[1, 2, 3, 4, 5, 6].map((factor) => (
+          {[1, 2, 3, 4, 6, 8, 10].map((factor) => (
             <label
               key={factor}
               className={
