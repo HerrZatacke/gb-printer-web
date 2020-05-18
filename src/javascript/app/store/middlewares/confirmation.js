d228ad2414470fed9ec8906dbb756eacf898c056
Herr Zatacke <zatacke@gmail.com>
2020-05-18 20:05:45 +0200

    allow close modal with Escape

--- src/javascript/app/store/middlewares/confirmation.js
+++ src/javascript/app/store/middlewares/confirmation.js
@@ -1,37 +1,47 @@
-const confirmation = (store) => (next) => (action) => {
+const confirmation = (store) => {
 
-  const state = store.getState();
+  document.addEventListener('keyup', (ev) => {
+    if (ev.key === 'Escape') {
+      store.dispatch({
+        type: 'CLOSE_OVERLAY',
+      });
+    }
+  });
 
-  switch (action.type) {
-    case 'DELETE_IMAGE':
-      if (action.confirmed) {
-        next(action);
+  return (next) => (action) => {
+    const state = store.getState();
+
+    switch (action.type) {
+      case 'DELETE_IMAGE':
+        if (action.confirmed) {
+          next(action);
+          return;
+        }
+
+        store.dispatch({
+          type: 'SET_CONFIRMATION',
+          payload: {
+            message: 'Delete the image?',
+            originalAction: { confirmed: true, ...action },
+          },
+        });
         return;
-      }
+      case 'CONFIRM_CONFIRMATION':
+        store.dispatch({
+          type: 'CLEAR_CONFIRMATION',
+        });
+        store.dispatch(state.confirmation.originalAction);
+        return;
+      case 'DENY_CONFIRMATION':
+        store.dispatch({
+          type: 'CLEAR_CONFIRMATION',
+        });
+        return;
+      default:
+    }
 
-      store.dispatch({
-        type: 'SET_CONFIRMATION',
-        payload: {
-          message: 'Delete the image?',
-          originalAction: { confirmed: true, ...action },
-        },
-      });
-      return;
-    case 'CONFIRM_CONFIRMATION':
-      store.dispatch({
-        type: 'CLEAR_CONFIRMATION',
-      });
-      store.dispatch(state.confirmation.originalAction);
-      return;
-    case 'DENY_CONFIRMATION':
-      store.dispatch({
-        type: 'CLEAR_CONFIRMATION',
-      });
-      return;
-    default:
-  }
-
-  next(action);
+    next(action);
+  };
 };
 
 
