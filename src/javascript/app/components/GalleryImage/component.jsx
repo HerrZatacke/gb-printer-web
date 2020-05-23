4b2e0e97d4a490231cd0a02b2996870490d3676b
Herr Zatacke <zatacke@gmail.com>
2020-05-23 18:32:28 +0200

    Add editbutton to GalleryImage

--- src/javascript/app/components/GalleryImage/component.jsx
+++ src/javascript/app/components/GalleryImage/component.jsx
@@ -70,7 +70,7 @@ class GalleryImage extends React.Component {
         >
           {dayjs(this.props.created, dateFormat).format(dateFormatReadable)}
         </span>
-        <GalleryImageButtons hash={this.props.hash} buttons={this.state.isRGBN ? ['delete', 'download'] : ['download', 'delete', 'edit']} />
+        <GalleryImageButtons hash={this.props.hash} buttons={['download', 'delete', 'edit']} />
       </li>
     );
   }
