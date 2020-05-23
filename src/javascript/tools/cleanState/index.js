3c7b4fd31e10382d1e3e9500f96295b9ac1d170d
Herr Zatacke <zatacke@gmail.com>
2020-05-23 18:28:22 +0200

    Add cleanState function to allow for migration of older data

---
+++ src/javascript/tools/cleanState/index.js
@@ -0,0 +1,42 @@
+const DEFAULT_GREYS = [0xff, 0xaa, 0x55, 0x00];
+
+const cleanState = (initialState) => {
+  const palettesShorts = initialState.palettes.map(({ shortName }) => shortName);
+
+  const images = initialState.images
+    .map((image) => {
+
+      // image is a greyscale image
+      if (image.palette) {
+        // if palette does not exist, update image to use default (first of list)
+        return (!palettesShorts.includes(image.palette)) ?
+          { ...image, palette: palettesShorts[0] } :
+          image;
+      }
+
+      // image is a rgbn image
+      if (image.hashes) {
+        if (!image.RGBNPalette) {
+          return {
+            ...image,
+            RGBNPalette: {
+              r: DEFAULT_GREYS.slice(),
+              g: DEFAULT_GREYS.slice(),
+              b: DEFAULT_GREYS.slice(),
+              n: DEFAULT_GREYS.slice(),
+            },
+          };
+        }
+
+        return image;
+      }
+
+      // image is neither rgbn nor default???
+      return null;
+    })
+    .filter(Boolean);
+
+  return { ...initialState, images };
+};
+
+export default cleanState;
