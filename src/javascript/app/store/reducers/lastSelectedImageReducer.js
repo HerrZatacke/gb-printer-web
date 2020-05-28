97ad915df1af5bb301137cb8a24d9c641e2cb0ba
Herr Zatacke <zatacke@gmail.com>
2020-05-28 21:23:25 +0200

    Revert "on shift-select do not clear last selected image"
    This reverts commit f15e8350

--- src/javascript/app/store/reducers/lastSelectedImageReducer.js
+++ src/javascript/app/store/reducers/lastSelectedImageReducer.js
@@ -3,8 +3,10 @@ const lastSelectedImageReducer = (value = null, action) => {
     case 'IMAGE_SELECTION_ADD':
     case 'IMAGE_SELECTION_REMOVE':
       return action.payload;
+    case 'SET_CURRENTPAGE':
     case 'DELETE_IMAGE':
     case 'DELETE_IMAGES':
+    case 'SET_CURRENT_GALLERY_VIEW':
       return null;
     default:
       return value;
