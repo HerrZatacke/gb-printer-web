47bccce4c560692bb4800eb0a21a13bcff3f327b
Herr Zatacke <zatacke@gmail.com>
2020-05-29 21:46:46 +0200

    refactor collecting image data before download

--- src/javascript/app/store/middlewares/startDownload.js
+++ src/javascript/app/store/middlewares/startDownload.js
@@ -1,36 +1,41 @@
 import { load } from '../../../tools/storage';
 import { prepareFiles } from '../../../tools/download';
 
+const loadImageData = ({ hash, hashes }) => {
+  if (!hashes) {
+    // default type image
+    return load(hash);
+  }
+
+  // rgbn image
+  return Promise.all([
+    load(hashes.r),
+    load(hashes.g),
+    load(hashes.b),
+    load(hashes.n),
+  ]);
+};
+
+const getImagePalette = ({ palettes }, { hashes, palette }) => (
+  (!hashes) ? palettes.find(({ shortName }) => shortName === palette) : palette
+);
+
 const startDownload = (store) => (next) => (action) => {
 
   if (action.type === 'START_DOWNLOAD') {
     const state = store.getState();
-    const image = state.images.find(({ hash }) => hash === action.payload);
     const exportScaleFactors = state.exportScaleFactors;
 
     if (exportScaleFactors.length === 0) {
       return null;
     }
 
-    if (!image.hashes) {
-      // default type image
-      const palette = state.palettes.find(({ shortName }) => shortName === image.palette);
-      load(action.payload)
-        .then(prepareFiles(palette, exportScaleFactors, image));
+    const image = state.images.find(({ hash }) => hash === action.payload);
+    const imagePalette = getImagePalette(state, image);
 
-    } else {
-      const palette = image.palette;
+    loadImageData(image)
+      .then(prepareFiles(imagePalette, exportScaleFactors, image));
 
-      // rgbn image
-      Promise.all([
-        load(image.hashes.r),
-        load(image.hashes.g),
-        load(image.hashes.b),
-        load(image.hashes.n),
-      ])
-        .then(prepareFiles(palette, exportScaleFactors, image));
-
-    }
   }
 
   return next(action);
