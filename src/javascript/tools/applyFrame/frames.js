fb880e4cf9d2c5079816e3a05b911d049b37a836
Herr Zatacke <zatacke@gmail.com>
2020-05-31 18:37:38 +0200

    move frames import to own file

---
+++ src/javascript/tools/applyFrame/frames.js
@@ -0,0 +1,62 @@
+import int01 from './int/01';
+import int02 from './int/02';
+import int03 from './int/03';
+import int04 from './int/04';
+import int05 from './int/05';
+import int06 from './int/06';
+import int07 from './int/07';
+import int08 from './int/08';
+import int09 from './int/09';
+import int10 from './int/10';
+import int11 from './int/11';
+import int12 from './int/12';
+import int13 from './int/13';
+import int14 from './int/14';
+import int15 from './int/15';
+import int16 from './int/16';
+import int17 from './int/17';
+import int18 from './int/18';
+import jp01 from './jp/01';
+import jp02 from './jp/02';
+import jp07 from './jp/07';
+import jp09 from './jp/09';
+
+export default {
+  int01,
+  int02,
+  int03,
+  int04,
+  int05,
+  int06,
+  int07,
+  int08,
+  int09,
+  int10,
+  int11,
+  int12,
+  int13,
+  int14,
+  int15,
+  int16,
+  int17,
+  int18,
+
+  jp01,
+  jp02,
+  jp03: int03,
+  jp04: int04,
+  jp05: int05,
+  jp06: int06,
+  jp07,
+  jp08: int08,
+  jp09,
+  jp10: int10,
+  jp11: int11,
+  jp12: int12,
+  jp13: int13,
+  jp14: int14,
+  jp15: int15,
+  jp16: int16,
+  jp17: int17,
+  jp18: int18,
+};
