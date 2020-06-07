760ce3311b2294edebb6d395e83e7d8747f08ff8
Herr Zatacke <zatacke@gmail.com>
2020-06-07 02:27:01 +0200

    inputfield to create a new tags

--- src/javascript/app/components/TagsSelect/component.jsx
+++ src/javascript/app/components/TagsSelect/component.jsx
@@ -3,43 +3,117 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import SVG from '../SVG';
 
-const TagsSelect = (props) => (
-  <ul className="tags-select">
-    {
-      props.tags.initial.map((tag) => (
+const uniqeTags = (tags) => (
+  tags.filter((tag, index) => (
+    tags.findIndex((findTag) => findTag === tag) === index
+  ))
+);
+
+class TagsSelect extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newTag: '',
+    };
+  }
+
+  addNewTag(tag) {
+    if (!tag) {
+      return;
+    }
+
+    this.props.updateTags('add', tag);
+    this.setState({
+      newTag: '',
+    });
+  }
+
+  render() {
+
+    const tags = uniqeTags([...this.props.tags.initial, ...this.props.tags.add]);
+
+    return (
+      <ul className="tags-select">
+        {
+          tags.map((tag) => (
+            <li
+              className={
+                classnames('tags-select__tag', {
+                  'tags-select__tag--add': this.props.tags.add.includes(tag),
+                  'tags-select__tag--remove': this.props.tags.remove.includes(tag),
+                })
+              }
+              key={tag}
+            >
+              <button
+                type="button"
+                className="tags-select__button tags-select__button--add"
+                onClick={() => this.props.updateTags('add', tag)}
+              >
+                <SVG name="add" />
+              </button>
+              <button
+                type="button"
+                className="tags-select__button tags-select__button--remove"
+                onClick={() => this.props.updateTags('remove', tag)}
+              >
+                <SVG name="remove" />
+              </button>
+              <span
+                className="tags-select__tag-name"
+              >
+                {tag}
+              </span>
+            </li>
+          ))
+        }
         <li
           className={
-            classnames('tags-select__tag', {
-              'tags-select__tag--add': props.tags.add.includes(tag),
-              'tags-select__tag--remove': props.tags.remove.includes(tag),
-            })
+            classnames('tags-select__tag')
           }
-          key={tag}
         >
           <button
             type="button"
             className="tags-select__button tags-select__button--add"
-            onClick={() => props.updateTags('add', tag)}
+            onClick={() => this.props.updateTags('add', this.state.newTag)}
           >
             <SVG name="add" />
           </button>
           <button
             type="button"
             className="tags-select__button tags-select__button--remove"
-            onClick={() => props.updateTags('remove', tag)}
+            onClick={() => this.props.updateTags('remove', this.state.newTag)}
           >
             <SVG name="remove" />
           </button>
-          <span
-            className="tags-select__tag-name"
-          >
-            {tag}
-          </span>
+          <input
+            type="text"
+            className="tags-select__tag-name tags-select__tag-name--input"
+            onChange={({ target }) => {
+              this.setState({
+                newTag: target.value,
+              });
+            }}
+            onBlur={({ target }) => {
+              this.addNewTag(target.value);
+            }}
+            onKeyUp={(ev) => {
+              switch (ev.key) {
+                case 'Enter':
+                  this.addNewTag(ev.target.value);
+                  break;
+                default:
+                  break;
+              }
+            }}
+            value={this.state.newTag}
+          />
         </li>
-      ))
-    }
-  </ul>
-);
+      </ul>
+    );
+  }
+}
 
 TagsSelect.propTypes = {
   tags: PropTypes.shape({
