66662689020e577a7f7e6e569026955d53607fbc
HerrZatacke <zatacke@gmail.com>
2021-02-01 14:36:57 +0100

    rework Import component to use hooks #16

--- src/javascript/app/components/Import/component.jsx
+++ src/javascript/app/components/Import/component.jsx
@@ -1,110 +1,104 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PrinterReport from '../PrinterReport';
 import { getEnv } from '../../../tools/getEnv';
 
-class Import extends React.Component {
+const Import = ({
+  importPlainText,
+  importFile,
+  checkPrinter,
+  dumpCount,
+  downloadPrinter,
+  clearPrinter,
+}) => {
+  const [text, setText] = useState('');
+  const [env, setEnv] = useState('');
 
-  constructor(props) {
-    super(props);
-    this.fileInputRef = React.createRef();
-    this.state = {
-      text: '',
-    };
+  useEffect(() => {
+    setEnv(getEnv());
 
-    this.env = getEnv();
-  }
-
-  componentDidMount() {
     import(/* webpackChunkName: "dmy" */ './dummy')
       .then(({ default: dummyContent }) => {
-        this.setState({
-          text: dummyContent.join('\n'),
-        });
+        setText(dummyContent.join('\n'));
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="import">
-        {this.env.env !== 'esp8266' ? null : (
-          <div className="import__inputgroup import__buttongroup">
-            <button
-              type="button"
-              className="import__button import__button"
-              onClick={this.props.checkPrinter}
-            >
-              Check Printer
-            </button>
-            <button
-              type="button"
-              className="import__button import__button"
-              disabled={this.props.dumpCount === 0}
-              onClick={this.props.downloadPrinter}
-            >
-              {`Download ${this.props.dumpCount ? this.props.dumpCount : ''} Dumps`}
-            </button>
-            <button
-              type="button"
-              className="import__button import__button"
-              disabled={this.props.dumpCount === 0}
-              onClick={this.props.clearPrinter}
-            >
-              Clear Printer
-            </button>
-          </div>
-        )}
-        <PrinterReport />
-        <div className="import__inputgroup">
-          <label htmlFor="import-file" className="import__label">
-            Select file for import
-          </label>
-          <input
-            id="import-file"
-            className="import__input"
-            type="file"
-            onChange={({ target }) => {
-              if (target.files && target.files.length === 1) {
-                this.props.importFile(target);
-              }
-            }}
-            ref={this.fileInputRef}
-          />
-          <label
-            htmlFor="import-file"
-            className="import__button import__button--label"
-          >
-            Select
-          </label>
-        </div>
-        <div className="import__inputgroup import__inputgroup--column">
-          <label htmlFor="import-plaintext" className="import__label">
-            Paste your plaintext
-          </label>
-          <textarea
-            id="import-plaintext"
-            className="import__data"
-            value={this.state.text}
-            onChange={({ target }) => {
-              this.setState({
-                text: target.value,
-              });
-            }}
-          />
+  return (
+    <div className="import">
+      {(env && env.env !== 'esp8266') ? null : (
+        <div className="import__inputgroup import__buttongroup">
           <button
-            className="import__button"
             type="button"
-            onClick={() => {
-              this.props.importPlainText(this.state.text);
-            }}
+            className="import__button import__button"
+            onClick={checkPrinter}
           >
-            Import
+            Check Printer
+          </button>
+          <button
+            type="button"
+            className="import__button import__button"
+            disabled={dumpCount === 0}
+            onClick={downloadPrinter}
+          >
+            {`Download ${dumpCount || ''} Dumps`}
+          </button>
+          <button
+            type="button"
+            className="import__button import__button"
+            disabled={dumpCount === 0}
+            onClick={clearPrinter}
+          >
+            Clear Printer
           </button>
         </div>
+      )}
+      <PrinterReport />
+      <div className="import__inputgroup">
+        <label htmlFor="import-file" className="import__label">
+          Select file for import
+        </label>
+        <input
+          id="import-file"
+          className="import__input"
+          type="file"
+          onChange={({ target }) => {
+            if (target.files && target.files.length === 1) {
+              importFile(target);
+            }
+          }}
+        />
+        <label
+          htmlFor="import-file"
+          className="import__button import__button--label"
+        >
+          Select
+        </label>
       </div>
-    );
-  }
-}
+      <div className="import__inputgroup import__inputgroup--column">
+        <label htmlFor="import-plaintext" className="import__label">
+          Paste your plaintext
+        </label>
+        <textarea
+          id="import-plaintext"
+          className="import__data"
+          value={text}
+          onChange={({ target }) => {
+            setText(target.value);
+          }}
+        />
+        <button
+          className="import__button"
+          type="button"
+          onClick={() => {
+            importPlainText(text);
+          }}
+        >
+          Import
+        </button>
+      </div>
+    </div>
+  );
+};
 
 Import.propTypes = {
   dumpCount: PropTypes.number.isRequired,
