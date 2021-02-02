17d19841159357a01e73df3595040080ab11692e
HerrZatacke <zatacke@gmail.com>
2021-02-02 01:54:47 +0100

    Show error message on error in bin fine parsing

--- src/javascript/tools/transformBin/index.js
+++ src/javascript/tools/transformBin/index.js
@@ -2,15 +2,23 @@ const getTransformBin = (dispatch) => (data, filename) => {
   const transformed = [];
   let currentLine = '';
 
-  // for (let i = 0; i < 16; i += 1) {
-  for (let i = 0; i < 5760; i += 1) {
-    currentLine += ` ${data[i + 8].toString(16)
-      .padStart(2, '0')}`;
+  try {
+    // for (let i = 0; i < 16; i += 1) {
+    for (let i = 0; i < 5760; i += 1) {
+      currentLine += ` ${data[i + 8].toString(16)
+        .padStart(2, '0')}`;
 
-    if (i % 16 === 15) {
-      transformed.push(currentLine.trim());
-      currentLine = '';
+      if (i % 16 === 15) {
+        transformed.push(currentLine.trim());
+        currentLine = '';
+      }
     }
+  } catch (error) {
+    dispatch({
+      type: 'ERROR',
+      payload: `ERROR_IN_LOADED_BIN\n${filename}`,
+    });
+    return;
   }
 
   dispatch({
