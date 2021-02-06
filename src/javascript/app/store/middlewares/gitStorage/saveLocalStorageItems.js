a965c8bc1a5d0164f9d3e9b36bd5f23f1eb76d42
HerrZatacke <zatacke@gmail.com>
2021-02-06 23:20:24 +0100

    pad frame to be able to save it correctly from repo

--- src/javascript/app/store/middlewares/gitStorage/saveLocalStorageItems.js
+++ src/javascript/app/store/middlewares/gitStorage/saveLocalStorageItems.js
@@ -1,4 +1,5 @@
 import { load, save } from '../../../../tools/storage';
+import { loadFrameData, saveFrameData } from '../../../../tools/applyFrame/frameData';
 
 const saveLocalStorageItems = (octoClient) => ({ images, frames }) => (
   Promise.all([
@@ -24,10 +25,38 @@ const saveLocalStorageItems = (octoClient) => ({ images, frames }) => (
             });
         });
     }),
+    ...frames.map((frame) => {
+      const frameId = frame.name.match(/^[a-z]+[0-9]+/gi)[0];
+
+      // check if item exists locally
+      return loadFrameData(frameId)
+        .then((frameData) => {
+          // if frame exists locally, don't query from Repo
+          if (frameData) {
+            return frameId;
+          }
+
+          return octoClient.getBlob(frame.sha)
+            .then((blob) => {
+              const tiles = JSON.parse(blob, null, 2);
+              const black = Array(32).fill('f').join('');
+              const pad = Array(16).fill(black);
+
+              // tiles need to be padded with some lines that get stripped again when saving frame data
+              const paddedFrameData = [
+                ...tiles.upper,
+                ...Array(14).fill().map((_, index) => ([
+                  ...tiles.left[index],
+                  ...pad,
+                  ...tiles.right[index],
+                ])).flat(),
+                ...tiles.lower,
+              ];
+              return saveFrameData(frameId, paddedFrameData);
+            });
+        });
+    }),
   ])
-    .then((stored) => {
-      console.log(stored);
-    })
 );
 
 export default saveLocalStorageItems;
