4b1d6fdafc11d5f25ef8334e91f3690118dbf727
HerrZatacke <zatacke@gmail.com>
2021-02-07 01:36:00 +0100

    make the whole gitStorage Middleware be downloaded only if configured

--- src/javascript/app/store/middlewares/gitStorage/index.js
+++ src/javascript/app/store/middlewares/gitStorage/index.js
@@ -1,68 +1,50 @@
-import OctoClient from '../../../../tools/OctoClient';
-import getUploadImages from './getUploadImages';
-import prepareGitFiles from './prepareGitFiles';
-import filterDeleteNew from './filterDeleteNew';
-import saveLocalStorageItems from './saveLocalStorageItems';
+// import { init, middleware } from './middleware';
 
 const gitStorage = (store) => {
+
+  let middleware = null;
+  let updateClient = null;
+  let initialized = false;
+
   const { gitStorage: gitStorageSettings } = store.getState();
 
-  const octoClient = new OctoClient(gitStorageSettings);
+  const {
+    use,
+    owner,
+    repo,
+    branch,
+    token,
+  } = gitStorageSettings;
 
-  octoClient.on('progress', (progress) => {
-    store.dispatch({
-      type: 'GITSTORAGE_PROGRESS',
-      payload: progress,
-    });
-  });
+  if (use && owner && repo && branch && token) {
+    import(/* webpackChunkName: "gmw" */ './middleware')
+      .then(({ init, updateClient: uc, middleware: mw }) => {
+        middleware = mw(store);
+        updateClient = uc;
+        init(store);
+        initialized = true;
+      });
+  }
 
   return (next) => (action) => {
 
     if (action.type === 'SET_GIT_STORAGE') {
-      octoClient.setOctokit(action.payload);
+      if (!initialized) {
+        import(/* webpackChunkName: "gmw" */ './middleware')
+          .then(({ init, updateClient: uc, middleware: mw }) => {
+            middleware = mw(store);
+            updateClient = uc;
+            init(store);
+            updateClient(action.payload);
+            initialized = true;
+          });
+      } else {
+        updateClient(action.payload);
+      }
     }
 
-    if (action.type === 'GITSTORAGE_SYNC_START') {
-      const state = store.getState();
-
-      octoClient.getRepoContents()
-        .then((repoContents) => {
-          switch (action.payload) {
-            case 'up':
-              return getUploadImages(state)
-                .then(({ missingLocally, imageCollection }) => {
-                  const gitFiles = prepareGitFiles(imageCollection);
-                  return filterDeleteNew(repoContents, gitFiles, missingLocally);
-                })
-                .then((changes) => (
-                  octoClient.updateRemoteStore(changes)
-                ));
-            case 'down':
-              return saveLocalStorageItems(octoClient)(repoContents)
-                .then(() => {
-                  store.dispatch({
-                    type: 'SETTINGS_IMPORT',
-                    payload: repoContents.settings,
-                  });
-                });
-            default:
-              return Promise.reject(new Error('wrong sync case'));
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          store.dispatch({
-            type: 'ERROR',
-            payload: error.message,
-          });
-          return error.message;
-        })
-        .then((syncResult) => {
-          store.dispatch({
-            type: 'GITSTORAGE_SYNC_DONE',
-            payload: syncResult,
-          });
-        });
+    if (middleware) {
+      middleware(action);
     }
 
     next(action);
