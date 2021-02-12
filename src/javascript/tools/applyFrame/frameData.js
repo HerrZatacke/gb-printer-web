6c6fdedb0266739c7c2293223abee2790ec9d7a2
HerrZatacke <zatacke@gmail.com>
2021-02-12 17:24:38 +0100

    load framedata from lofo

--- src/javascript/tools/applyFrame/frameData.js
+++ src/javascript/tools/applyFrame/frameData.js
@@ -26,34 +26,29 @@ const loadFrameData = (frameId) => {
     return Promise.resolve(null);
   }
 
-  /* todo */let binary = localStorage.getItem(`gbp-web-frame-${frameId}`);
-
-  if (!binary) {
-    /* todo */binary = localStorage.getItem(`gbp-web-frame-base64-${frameId}`);
-    if (binary) {
-      binary = atob(binary);
-    }
-  }
-
-  if (!binary) {
-    return Promise.resolve(null);
-  }
-
-  return import(/* webpackChunkName: "pko" */ 'pako')
-    .then(({ default: pako }) => {
-      try {
-        const tiles = pako.inflate(binary, { to: 'string' }).split('\n');
-
-        return {
-          upper: tiles.slice(0, 40),
-          left: Array(14).fill(0).map((_, index) => tiles.slice((index * 4) + 40, (index * 4) + 42)),
-          right: Array(14).fill(0).map((_, index) => tiles.slice((index * 4) + 42, (index * 4) + 44)),
-          lower: tiles.slice(96, 136),
-        };
-      } catch (error) {
+  return localforageFrames.getItem(frameId)
+    .then((binary) => {
+      if (!binary) {
         return null;
       }
+
+      return import(/* webpackChunkName: "pko" */ 'pako')
+        .then(({ default: pako }) => {
+          try {
+            const tiles = pako.inflate(binary, { to: 'string' }).split('\n');
+
+            return {
+              upper: tiles.slice(0, 40),
+              left: Array(14).fill(0).map((_, index) => tiles.slice((index * 4) + 40, (index * 4) + 42)),
+              right: Array(14).fill(0).map((_, index) => tiles.slice((index * 4) + 42, (index * 4) + 44)),
+              lower: tiles.slice(96, 136),
+            };
+          } catch (error) {
+            return null;
+          }
+        });
     });
+
 };
 
 export {
