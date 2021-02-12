a159371f6a309fa3d4765f9818d9bebcb6df1996
HerrZatacke <zatacke@gmail.com>
2021-02-12 15:16:43 +0100

    install localforage

--- src/javascript/tools/getEnv/index.js
+++ src/javascript/tools/getEnv/index.js
@@ -1,3 +1,10 @@
+import localforage from 'localforage';
+
+localforage.config({
+  name: 'GB Printer Web',
+  storeName: 'gb-printer-web',
+});
+
 let envData = null;
 
 const loadEnv = () => {
@@ -5,19 +12,26 @@ const loadEnv = () => {
     return Promise.resolve(envData);
   }
 
-  return fetch('./env.json')
-    .then((res) => res.json())
-    .catch(() => ({
-      version: '0.0.0',
-      maximages: 0,
-      env: 'error',
-      fstype: '-',
-      bootmode: '-',
-      oled: false,
-    }))
-    .then((env) => {
-      envData = env;
-    });
+  return localforage.ready()
+    .then(() => (
+      fetch('./env.json')
+        .then((res) => res.json())
+        .catch(() => ({
+          version: '0.0.0',
+          maximages: 0,
+          localforage: 'error',
+          env: 'error',
+          fstype: '-',
+          bootmode: '-',
+          oled: false,
+        }))
+        .then((env) => {
+          envData = {
+            ...env,
+            localforage: localforage.driver(), // localStorageWrapper or asyncStorage or webSQLStorage
+          };
+        })
+    ));
 };
 
 const getEnv = () => envData;
