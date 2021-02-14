2e0f83a6fae02b414eb63b033d5d129f1a015fce
HerrZatacke <zatacke@gmail.com>
2021-02-14 19:26:18 +0100

    themecolors for gallery pagination

--- src/javascript/app/components/PaginationButton/component.jsx
+++ src/javascript/app/components/PaginationButton/component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 
 const PaginationButton = (props) => (
   <li
-    className={props.className}
+    className={classNames('gallery-button pagination__button', {
+      'gallery-button--selected': props.active,
+    })}
   >
     <NavLink
       className="pagination__link"
@@ -19,14 +22,14 @@ const PaginationButton = (props) => (
 
 PaginationButton.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
+  active: PropTypes.bool,
   disabled: PropTypes.bool,
   page: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
 };
 
 PaginationButton.defaultProps = {
-  className: null,
+  active: false,
   disabled: false,
 };
 
