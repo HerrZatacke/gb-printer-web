861e7963353272f1271bea5a0ff95c9b9f7f5242
HerrZatacke <zatacke@gmail.com>
2021-02-16 20:00:02 +0100

    use metadata of imported images if duplicate when importing JSON

--- src/javascript/tools/mergeStates/index.js
+++ src/javascript/tools/mergeStates/index.js
@@ -7,15 +7,15 @@ const mergeStates = (currentState, updatedState, mergeImagesFrames) => {
 
   if (mergeImagesFrames) {
     if (updatedState.frames && updatedState.frames.length) {
-      frames.push(...updatedState.frames);
+      frames.unshift(...updatedState.frames);
     }
 
     if (updatedState.images && updatedState.images.length) {
-      images.push(...updatedState.images);
+      images.unshift(...updatedState.images);
     }
 
     // if (updatedState.palettes && updatedState.palettes.length) {
-    //   palettes.push(...updatedState.palettes);
+    //   palettes.unshift(...updatedState.palettes);
     // }
   } else {
     frames = updatedState.frames || currentState.frames;
