63974be3013eeb9fc2f88f8ee4bc7c7e6d16f9ce
HerrZatacke <zatacke@gmail.com>
2021-02-20 19:43:31 +0100

    output branch info (git <2.2)

--- scripts/getBranch/index.js
+++ scripts/getBranch/index.js
@@ -2,7 +2,11 @@ const { execSync } = require('child_process');
 
 module.exports = () => {
   try {
-    return execSync('git branch --show-current').toString('ascii').trim();
+    return execSync('git branch')
+      .toString('ascii')
+      .split('*')
+      .pop()
+      .trim();
   } catch (error) {
     return 'n/a';
   }
