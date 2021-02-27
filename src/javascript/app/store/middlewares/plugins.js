ecd21243320919d296c39026fa0ba4601b353d68
HerrZatacke <zatacke@gmail.com>
2021-02-27 18:40:22 +0100

    basic functions to pass imagedata to a plugin

--- src/javascript/app/store/middlewares/plugins.js
+++ src/javascript/app/store/middlewares/plugins.js
@@ -1,3 +1,8 @@
+import loadImageTiles from '../../../tools/loadImageTiles';
+import getImagePalette from '../../../tools/getImagePalette';
+import RGBNDecoder from '../../../tools/RGBNDecoder';
+import Decoder from '../../../tools/Decoder';
+
 const pluginsMiddleware = (store) => {
   const registeredPlugins = {};
 
@@ -10,8 +15,16 @@ const pluginsMiddleware = (store) => {
         try {
           const instance = new Plugin({ store });
           instance.init();
-          const name = instance.name;
-          registeredPlugins[url] = { instance, name };
+          const { name, description } = instance;
+          registeredPlugins[url] = instance;
+          store.dispatch({
+            type: 'PLUGIN_UPDATE_PROPERTIES',
+            payload: {
+              url,
+              name,
+              description,
+            },
+          });
         } catch (error) {
           store.dispatch({
             type: 'ERROR',
@@ -25,13 +38,58 @@ const pluginsMiddleware = (store) => {
       document.head.appendChild(pluginScript);
       pluginScript.src = url;
     });
-
   });
+
+  const collectImageData = (hash) => {
+    const state = store.getState();
+    const meta = state.images.find((image) => image.hash === hash);
+    const palette = getImagePalette(state, meta);
+    const getTiles = () => loadImageTiles(meta, state);
+
+    const getCanvas = (scaleFactor = 1, cropFrame = false) => (
+      getTiles()
+        .then((tiles) => {
+          const isRGBN = !!meta.hashes;
+          const decoder = isRGBN ? new RGBNDecoder() : new Decoder();
+          const lockFrame = meta.lockFrame || false;
+          const invertPalette = meta.invertPalette || false;
+
+          if (isRGBN) {
+            decoder.update({
+              tiles: RGBNDecoder.rgbnTiles(tiles),
+              palette,
+              lockFrame,
+            });
+          } else {
+            decoder.update({
+              tiles,
+              palette: palette.palette,
+              lockFrame,
+              invertPalette,
+            });
+          }
+
+          return decoder.getScaledCanvas(scaleFactor, cropFrame);
+        })
+    );
+
+    return {
+      meta,
+      palette,
+      getTiles,
+      getCanvas,
+    };
+  };
 
   return (next) => (action) => {
 
     switch (action.type) {
-      case 'PLUGIN_IMAGE':
+      case 'PLUGIN_IMAGE': {
+        const { url, hash } = action.payload;
+        registeredPlugins[url].withImage(collectImageData(hash));
+        break;
+      }
+
       case 'PLUGIN_IMAGES':
       default:
         break;
