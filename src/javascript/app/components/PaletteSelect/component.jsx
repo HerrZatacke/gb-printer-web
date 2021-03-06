a50aa1c4a3d141e8d2c3e0bb2a635fc3d15e9df8
HerrZatacke <zatacke@gmail.com>
2021-03-06 22:05:07 +0100

    #16 for PaletteSelect

--- src/javascript/app/components/PaletteSelect/component.jsx
+++ src/javascript/app/components/PaletteSelect/component.jsx
@@ -1,116 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import SVG from '../SVG';
 
-class PaletteSelect extends React.Component {
+const PaletteSelect = (props) => {
 
-  constructor(props) {
-    super(props);
+  const [initiallySelected, setInitiallySelected] = useState(props.value);
 
-    this.state = {
-      initiallySelected: props.value,
-    };
+  const palettes = [...props.palettes];
+
+  // this option is used for assigning a single palette to an animation
+  if (props.allowEmpty) {
+    palettes.unshift({
+      shortName: '',
+      name: 'As selected per image',
+      palette: [],
+    });
   }
 
-  render() {
-    const palettes = [...this.props.palettes];
-
-    // this option is used for assigning a single palette to an animation
-    if (this.props.allowEmpty) {
-      palettes.unshift({
-        shortName: '',
-        name: 'As selected per image',
-        palette: [],
-      });
-    }
-
-    return (
-      <>
-        <select
-          className="palette-select"
-          value={this.props.value}
-          onChange={(ev) => {
-            this.props.onChange(ev.target.value, true);
-            this.setState({
-              initiallySelected: ev.target.value,
-            });
+  return (
+    <>
+      <select
+        className="palette-select"
+        value={props.value}
+        onChange={(ev) => {
+          props.onChange(ev.target.value, true);
+          setInitiallySelected(ev.target.value);
+        }}
+      >
+        {
+          palettes.map(({ shortName, name }) => (
+            <option
+              key={shortName}
+              value={shortName}
+            >
+              { name }
+            </option>
+          ))
+        }
+      </select>
+      <label
+        className={
+          classnames('palette-select__check-label', {
+            'palette-select__check-label--checked': props.invertPalette,
+          })
+        }
+      >
+        <input
+          type="checkbox"
+          className="palette-select__checkbox"
+          checked={props.invertPalette}
+          onChange={({ target }) => {
+            props.updateInvertPalette(target.checked);
           }}
-        >
+        />
+        <SVG name="checkmark" />
+        <span className="palette-select__check-label-text">
+          Invert Palette
+        </span>
+      </label>
+      { props.noFancy ? null : (
+        <ul className="fancy-palette-select">
           {
-            palettes.map(({ shortName, name }) => (
-              <option
+            palettes.map(({ shortName, name, palette }) => (
+              <li
                 key={shortName}
-                value={shortName}
+                className={
+                  classnames('fancy-palette-select__entry', {
+                    'fancy-palette-select__entry--active': initiallySelected === shortName,
+                  })
+                }
               >
-                { name }
-              </option>
+                <button
+                  type="button"
+                  className="fancy-palette-select__button"
+                  title={name}
+                  onClick={() => {
+                    props.onChange(shortName, true);
+                    setInitiallySelected(shortName);
+                  }}
+                  onMouseEnter={() => {
+                    props.onChange(shortName, false);
+                  }}
+                  onMouseLeave={() => {
+                    props.onChange(initiallySelected);
+                  }}
+                >
+                  <span className="fancy-palette-select__color" style={{ backgroundColor: palette[0] }} />
+                  <span className="fancy-palette-select__color" style={{ backgroundColor: palette[1] }} />
+                  <span className="fancy-palette-select__color" style={{ backgroundColor: palette[2] }} />
+                  <span className="fancy-palette-select__color" style={{ backgroundColor: palette[3] }} />
+                </button>
+              </li>
             ))
           }
-        </select>
-        <label
-          className={
-            classnames('palette-select__check-label', {
-              'palette-select__check-label--checked': this.props.invertPalette,
-            })
-          }
-        >
-          <input
-            type="checkbox"
-            className="palette-select__checkbox"
-            checked={this.props.invertPalette}
-            onChange={({ target }) => {
-              this.props.updateInvertPalette(target.checked);
-            }}
-          />
-          <SVG name="checkmark" />
-          <span className="palette-select__check-label-text">
-            Invert Palette
-          </span>
-        </label>
-        { this.props.noFancy ? null : (
-          <ul className="fancy-palette-select">
-            {
-              palettes.map(({ shortName, name, palette }) => (
-                <li
-                  key={shortName}
-                  className={
-                    classnames('fancy-palette-select__entry', {
-                      'fancy-palette-select__entry--active': this.state.initiallySelected === shortName,
-                    })
-                  }
-                >
-                  <button
-                    type="button"
-                    className="fancy-palette-select__button"
-                    title={name}
-                    onClick={() => {
-                      this.props.onChange(shortName, true);
-                      this.setState({
-                        initiallySelected: shortName,
-                      });
-                    }}
-                    onMouseEnter={() => {
-                      this.props.onChange(shortName, false);
-                    }}
-                    onMouseLeave={() => {
-                      this.props.onChange(this.state.initiallySelected);
-                    }}
-                  >
-                    <span className="fancy-palette-select__color" style={{ backgroundColor: palette[0] }} />
-                    <span className="fancy-palette-select__color" style={{ backgroundColor: palette[1] }} />
-                    <span className="fancy-palette-select__color" style={{ backgroundColor: palette[2] }} />
-                    <span className="fancy-palette-select__color" style={{ backgroundColor: palette[3] }} />
-                  </button>
-                </li>
-              ))
-            }
-          </ul>
-        )}
-      </>
-    );
-  }
-}
+        </ul>
+      )}
+    </>
+  );
+};
 
 PaletteSelect.propTypes = {
   palettes: PropTypes.array.isRequired,
