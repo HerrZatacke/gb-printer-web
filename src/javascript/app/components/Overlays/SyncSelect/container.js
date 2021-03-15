be45a2743d8925aa8120babaa0fab280b39bd5a5
HerrZatacke <zatacke@gmail.com>
2021-03-15 01:36:09 +0100

    optimize dropbox sync ui and flow

--- src/javascript/app/components/Overlays/SyncSelect/container.js
+++ src/javascript/app/components/Overlays/SyncSelect/container.js
@@ -2,12 +2,33 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => ({
   repoUrl: `https://github.com/${state.gitStorage.owner}/${state.gitStorage.repo}/tree/${state.gitStorage.branch}`,
+  dropboxActive: !!(
+    state.dropboxStorage.use &&
+    state.dropboxStorage.accessToken
+  ),
+  gitActive: !!(
+    state.gitStorage.use &&
+    state.gitStorage.owner &&
+    state.gitStorage.repo &&
+    state.gitStorage.branch &&
+    state.gitStorage.throttle &&
+    state.gitStorage.token
+  ),
 });
 
 const mapDispatchToProps = (dispatch) => ({
   startSync: (storageType, direction) => {
     dispatch({
       type: 'STORAGE_SYNC_START',
+      payload: {
+        storageType,
+        direction,
+      },
+    });
+  },
+  cancelSync: (storageType, direction) => {
+    dispatch({
+      type: 'STORAGE_SYNC_CANCEL',
       payload: {
         storageType,
         direction,
