fa5a9381f5696622b6cdb46f7fa8036680b8d54e
HerrZatacke <zatacke@gmail.com>
2021-03-19 19:06:23 +0100

    add new special tag for "recently imported"

--- src/javascript/consts/specialTags.js
+++ src/javascript/consts/specialTags.js
@@ -2,4 +2,5 @@ export const FILTER_UNTAGGED = '__filter:untagged__';
 export const FILTER_NEW = '__filter:new__';
 export const FILTER_MONOCHROME = '__filter:mono__';
 export const FILTER_RGB = '__filter:rgb__';
+export const FILTER_RECENT = '__filter:recent__';
 export const NEW_PALETTE_SHORT = '__new:palette__';
