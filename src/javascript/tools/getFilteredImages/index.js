ad6a88981acb31ed5aa077e9bb3a1e382c6d5ea9
HerrZatacke <zatacke@gmail.com>
2021-03-19 19:11:26 +0100

    add filtering for recent imported images

--- src/javascript/tools/getFilteredImages/index.js
+++ src/javascript/tools/getFilteredImages/index.js
@@ -1,8 +1,8 @@
 import sortImages from '../sortImages';
 import filter from './filter';
 
-const getFilteredImages = ({ images: stateImages, filter: { activeTags }, sortBy }) => (
-  [...stateImages].sort(sortImages({ sortBy })).filter(filter(activeTags))
+const getFilteredImages = ({ images: stateImages, filter: { activeTags }, sortBy, recentImports }) => (
+  [...stateImages].sort(sortImages({ sortBy })).filter(filter(activeTags, recentImports))
 );
 
 export default getFilteredImages;
