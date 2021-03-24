ee3c5e7d4d0312e3f37deb3f13e2f0c1b1c5d525
HerrZatacke <zatacke@gmail.com>
2021-03-25 00:12:31 +0100

    notify about duplicates in batch imports

--- src/javascript/app/store/middlewares/importQueue.js
+++ src/javascript/app/store/middlewares/importQueue.js
@@ -28,7 +28,7 @@ const importQueue = (store) => {
         store.dispatch({
           type: 'CONFIRM_ASK',
           payload: {
-            message: `Import ${uniqueResult.length} images? ${duplicatesMsg}`,
+            message: `Import ${uniqueResult.length} images?${duplicatesMsg}`,
             confirm: () => {
               store.dispatch({
                 type: 'ADD_IMAGES',
@@ -44,35 +44,6 @@ const importQueue = (store) => {
         });
       });
   };
-
-
-  // const images = [];
-  // let queueRunning;
-  //
-  // const startQueue = () => {
-  //   if (queueRunning || !images.length) {
-  //     return;
-  //   }
-  //
-  //   queueRunning = true;
-  //
-  //   window.setTimeout(() => {
-  //     queueRunning = false;
-  //     const image = images.shift();
-  //
-  //     saveNewImage({
-  //       lines: image.lines,
-  //       filename: image.file,
-  //       palette: store.getState().activePalette,
-  //       dispatch: store.dispatch,
-  //     });
-  //   }, 50);
-  //
-  //   store.dispatch({
-  //     type: 'IMPORT_QUEUE_SIZE',
-  //     payload: images.length - 1,
-  //   });
-  // };
 
   return (next) => (action) => {
 
