0eae812fbb52609b21f4f11e1699eb2438fb0a99
HerrZatacke <zatacke@gmail.com>
2021-03-27 16:42:18 +0100

    move useQuestions.js to hooks folder

--- src/javascript/app/components/Overlays/Confirm/component.jsx
+++ src/javascript/app/components/Overlays/Confirm/component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Lightbox from '../../Lightbox';
-import useQuestions from './useQuestions';
+import useQuestions from '../../../../hooks/useQuestions';
 import Select from './types/Select';
 import Input from '../../Input';
 
