d382afeadd3542dd1f91df4cfa80fa866e602a21
HerrZatacke <zatacke@gmail.com>
2021-03-28 21:35:21 +0200

    add square options for exported images

--- src/javascript/app/components/Settings/pages/GenericSettings/component.jsx
+++ src/javascript/app/components/Settings/pages/GenericSettings/component.jsx
@@ -17,6 +17,18 @@ const exportFrameModes = [
     id: 'crop',
     name: 'Crop frame',
   },
+  {
+    id: 'square_black',
+    name: 'Make image squared (add black)',
+  },
+  {
+    id: 'square_white',
+    name: 'Make image squared (add white)',
+  },
+  {
+    id: 'square_smart',
+    name: 'Make image squared (smart - repeats first/last line - works in most cases...)',
+  },
 ];
 
 const GenericSettings = (props) => {
