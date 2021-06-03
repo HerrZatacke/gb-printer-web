b81427cf9a78682bd5551fcd1db7076e310c02f9
HerrZatacke <zatacke@gmail.com>
2021-06-03 03:14:21 +0200

    dont fail on image import if frameID is undefined

--- src/javascript/tools/transformBitmap/getFrameId.js
+++ src/javascript/tools/transformBitmap/getFrameId.js
@@ -1,4 +1,4 @@
-const getFrameId = ({ frameSetNew, frameSet, frameIndex }) => {
+const getFrameId = ({ frameSetNew, frameSet, frameIndex = '' }) => {
   const id = `${frameSetNew || frameSet}${frameIndex.padStart(2, '0')}`;
   return id.match(/^[a-z]{2,}\d{2}$/g) ? id : '';
 };
