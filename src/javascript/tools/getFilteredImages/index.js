47ccc5d9596fdc470377767a60f62bb406ef8b63
HerrZatacke <zatacke@gmail.com>
2021-07-27 04:34:31 +0200

    Revert "remove non-existant tags from active tags before filtering"
    This reverts commit ce7c2a81

--- src/javascript/tools/getFilteredImages/index.js
+++ src/javascript/tools/getFilteredImages/index.js
@@ -1,20 +1,12 @@
 import { addSortIndex, removeSortIndex, sortImages } from '../sortImages';
 import filter from './filter';
-import { getAvailableTags } from '../../hooks/useAvailableTags';
 
-// eslint-disable-next-line arrow-body-style
-const getFilteredImages = ({ images: stateImages, filtersActiveTags, sortBy, recentImports }) => {
-
-  const availableTags = getAvailableTags(stateImages);
-  const filterTags = filtersActiveTags.filter((tag) => availableTags.includes(tag));
-
-  return (
-    [...stateImages]
-      .map(addSortIndex)
-      .sort(sortImages({ sortBy }))
-      .map(removeSortIndex)
-      .filter(filter(filterTags, recentImports))
-  );
-};
+const getFilteredImages = ({ images: stateImages, filtersActiveTags, sortBy, recentImports }) => (
+  [...stateImages]
+    .map(addSortIndex)
+    .sort(sortImages({ sortBy }))
+    .map(removeSortIndex)
+    .filter(filter(filtersActiveTags, recentImports))
+);
 
 export default getFilteredImages;
