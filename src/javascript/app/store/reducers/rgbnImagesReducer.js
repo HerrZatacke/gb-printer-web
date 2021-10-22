e0cb0aabd962d2e66b9b2d0b5e904c5eee32e744
HerrZatacke <zatacke@gmail.com>
2021-10-22 15:33:49 +0200

    fix: overlay menus not working if creating a rgbn image is cancelled

--- src/javascript/app/store/reducers/rgbnImagesReducer.js
+++ src/javascript/app/store/reducers/rgbnImagesReducer.js
@@ -2,8 +2,11 @@ import updateIfDefined from '../../../tools/updateIfDefined';
 
 const rgbnImagesReducer = (value = null, action) => {
   switch (action.type) {
-    case 'UPDATE_RGBN_PART':
-      return { ...value, ...action.payload };
+    case 'UPDATE_RGBN_PART': {
+      const newVal = { ...value, ...action.payload };
+      return newVal.r || newVal.g || newVal.b || newVal.n ? newVal : null;
+    }
+
     case 'ADD_IMAGES':
       return action.payload[0]?.hashes ? null : value;
     case 'DELETE_IMAGE':
