62abd61232539c8174a68189ba7d40e04e8b66de
HerrZatacke <zatacke@gmail.com>
2021-11-07 21:12:01 +0100

    disable warnings overlay

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -13,6 +13,11 @@ module.exports = merge(common(), {
     static: {
       directory: path.join(process.cwd(), 'src', 'assets'),
     },
+    client: {
+      overlay: {
+        warnings: false,
+      },
+    },
     port: 3000,
     host: '0.0.0.0',
     onBeforeSetupMiddleware: ({ app }) => setupServer(app),
