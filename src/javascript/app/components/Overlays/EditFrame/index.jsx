43ac93d985b21460077345865e80656529b3d015
Andreas Hahn <zatacke@gmail.com>
2022-09-04 17:15:04 +0200

    Add dropdown with existing framesets when editing frames

--- src/javascript/app/components/Overlays/EditFrame/index.jsx
+++ src/javascript/app/components/Overlays/EditFrame/index.jsx
@@ -22,6 +22,8 @@ const EditFrame = () => {
 
   const updateHead = updateId !== fullId ? ` -> "${fullId}"` : '';
 
+  const groupExists = Boolean(groups.find(({ id }) => (frameGroup === id)));
+
   return (
     <Lightbox
       className="edit-frame"
@@ -34,11 +36,33 @@ const EditFrame = () => {
       <div
         className="edit-frame__content"
       >
+        <div className="inputgroup">
+          <label htmlFor="frame-edit-group" className="inputgroup__label">
+            Frame group
+          </label>
+          <select
+            id="frame-edit-group"
+            className="inputgroup__input inputgroup__input--select"
+            value={frameGroup}
+            onChange={(ev) => {
+              setFrameGroup(ev.target.value);
+            }}
+          >
+            <option value="">
+              {groupExists ? 'Select Frameset' : 'New Frameset'}
+            </option>
+            {
+              groups.map(({ id, name }) => (
+                <option value={id} key={id}>{ name }</option>
+              ))
+            }
+          </select>
+        </div>
         <Input
-          id="frame-edit-group"
-          labelText="Frame group"
+          id="frame-edit-new-group"
+          labelText="New group"
           type="text"
-          value={frameGroup}
+          value={groupExists ? '' : frameGroup}
           onChange={setFrameGroup}
         />
         <Input
