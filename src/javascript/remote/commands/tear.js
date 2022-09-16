1efa32ff3712187d25c446cca35321852c3dbe60
Andreas Hahn <zatacke@gmail.com>
2022-09-16 18:11:59 +0200

    Fix "tear" endpoint address

--- src/javascript/remote/commands/tear.js
+++ src/javascript/remote/commands/tear.js
@@ -1,7 +1,7 @@
 import checkPrinter from './checkPrinter';
 
 const clearPrinter = () => (
-  fetch('/tear')
+  fetch('/reset')
     .then((res) => res.json())
     .then(({ result }) => {
       if (result === 'ok') {
