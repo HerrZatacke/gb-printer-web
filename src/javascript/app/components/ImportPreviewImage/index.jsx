908ea481fa2411090d82e94664245648f3156ec9
Andreas Hahn <zatacke@gmail.com>
2022-12-28 17:09:56 +0100

    Add Scale Factor Message to ImportPreviewImage

--- src/javascript/app/components/ImportPreviewImage/index.jsx
+++ src/javascript/app/components/ImportPreviewImage/index.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import isGoodScaleFactor from '../../../tools/isGoodScaleFactor';
 import { applyBitmapFilter } from '../../../tools/applyBitmapFilter';
 import SVG from '../SVG';
 import './index.scss';
+import InfoText from '../InfoText';
 
 const ImportPreviewImage = ({
   imageData,
@@ -30,7 +32,6 @@ const ImportPreviewImage = ({
     }
   });
 
-  // ToDo: messages for Scalefactor
   return (
     <div
       className="import-preview-image"
@@ -52,6 +53,14 @@ const ImportPreviewImage = ({
         height={height}
         ref={canvas}
       />
+      {
+        isGoodScaleFactor(scaleFactor) ? null : (
+          <InfoText
+            label={`The scale factor of your image is ${scaleFactor.toPrecision(3)}. To get a clean result without artifacts, use images with factors being powers of two. (1, 2, 4, 8 ...)`}
+            themes={['warning']}
+          />
+        )
+      }
     </div>
   );
 };
