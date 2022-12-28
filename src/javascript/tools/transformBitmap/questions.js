9a7159a9a34eeabb9163e8613dd696fe40eb5d58
Andreas Hahn <zatacke@gmail.com>
2022-12-28 16:51:49 +0100

    move isGoodScaleFactor to own file

--- src/javascript/tools/transformBitmap/questions.js
+++ src/javascript/tools/transformBitmap/questions.js
@@ -1,10 +1,5 @@
 import getFrameId from './getFrameId';
-
-// noinspection JSBitwiseOperatorUsage
-const isPowerOfTwo = (v) => (
-  // eslint-disable-next-line no-bitwise
-  v && !(v & (v - 1))
-);
+import isGoodScaleFactor from '../isGoodScaleFactor';
 
 const getQuestions = ({ frameIds, frameGroups, fileName, scaleFactor = 1 }) => ({
   frameSet = '',
@@ -25,13 +20,8 @@ const getQuestions = ({ frameIds, frameGroups, fileName, scaleFactor = 1 }) => (
     (!frameId && !frameName)
   ) || !frameSetNewFormatOk;
 
-  const isGoodScaleFactor = (
-    isPowerOfTwo(scaleFactor) &&
-    Math.floor(scaleFactor) === scaleFactor
-  );
-
   return [
-    isGoodScaleFactor ? null : {
+    isGoodScaleFactor(scaleFactor) ? null : {
       label: `The scale factor of your image is ${scaleFactor.toPrecision(3)}. To get a clean result without artifacts, use images with factors being powers of two. (1, 2, 4, 8 ...)`,
       key: 'badScaleFactor',
       type: 'info',
