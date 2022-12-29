72a3c9d27f58268dd42e97b6c053303078250148
Andreas Hahn <zatacke@gmail.com>
2022-12-29 16:01:53 +0100

    Make lockFrame option in FrameSelect optional

--- src/javascript/app/components/FrameSelect/component.jsx
+++ src/javascript/app/components/FrameSelect/component.jsx
@@ -21,26 +21,30 @@ const FrameSelect = (props) => (
         ))
       }
     </select>
-    <label
-      className={
-        classnames('frame-select__check-label', {
-          'frame-select__check-label--checked': props.lockFrame,
-        })
-      }
-    >
-      <input
-        type="checkbox"
-        className="frame-select__checkbox"
-        checked={props.lockFrame}
-        onChange={({ target }) => {
-          props.updateFrameLock(target.checked);
-        }}
-      />
-      <SVG name="checkmark" />
-      <span className="frame-select__check-label-text">
-        Palette does not affect frame
-      </span>
-    </label>
+    {
+      (typeof props.updateFrameLock === 'function') ? (
+        <label
+          className={
+            classnames('frame-select__check-label', {
+              'frame-select__check-label--checked': props.lockFrame,
+            })
+          }
+        >
+          <input
+            type="checkbox"
+            className="frame-select__checkbox"
+            checked={props.lockFrame}
+            onChange={({ target }) => {
+              props.updateFrameLock(target.checked);
+            }}
+          />
+          <SVG name="checkmark" />
+          <span className="frame-select__check-label-text">
+            Palette does not affect frame
+          </span>
+        </label>
+      ) : null
+    }
   </>
 );
 
@@ -48,13 +52,15 @@ FrameSelect.propTypes = {
   frame: PropTypes.string.isRequired,
   frames: PropTypes.array.isRequired,
   updateFrame: PropTypes.func.isRequired,
-  updateFrameLock: PropTypes.func.isRequired,
-  lockFrame: PropTypes.bool.isRequired,
+  updateFrameLock: PropTypes.func,
+  lockFrame: PropTypes.bool,
   noFrameOption: PropTypes.string,
 };
 
 FrameSelect.defaultProps = {
   noFrameOption: 'As imported / No frame',
+  lockFrame: false,
+  updateFrameLock: null,
 };
 
 export default FrameSelect;
