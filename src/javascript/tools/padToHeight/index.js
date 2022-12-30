31591f52b017eb09299d22a8d52431422e65b878
Andreas Hahn <zatacke@gmail.com>
2022-12-30 17:34:22 +0100

    Optimize padding function / only pad with black tiles

--- src/javascript/tools/padToHeight/index.js
+++ src/javascript/tools/padToHeight/index.js
@@ -1,8 +1,21 @@
 
 const padToHeight = (tiles) => {
+  const pad = 'FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF';
+
+  // Fill up to a full line
+  while (tiles.length % 20) {
+    tiles.push(pad);
+  }
+
+  const padLine = (new Array(20)).fill(pad);
+
+  // eslint-disable-next-line
   while (tiles.length < 360) {
-    tiles.push(tiles[0]);
-    tiles.unshift(tiles[0]);
+    tiles.push(...padLine);
+
+    if (tiles.length < 360) {
+      tiles.unshift(...padLine);
+    }
   }
 
   return tiles;
