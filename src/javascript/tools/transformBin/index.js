22510860c65401ff7e8db8880ce76b9c2849c088
Andreas Hahn <zatacke@gmail.com>
2022-12-30 20:49:55 +0100

    Implement BIN and JSON import

--- src/javascript/tools/transformBin/index.js
+++ src/javascript/tools/transformBin/index.js
@@ -1,42 +1,65 @@
-import { ERROR } from '../../app/store/actions';
+import { IMPORTQUEUE_ADD } from '../../app/store/actions';
+import readFileAs from '../readFileAs';
+import { compressAndHash } from '../storage';
+import { compressAndHashFrame } from '../applyFrame/frameData';
 
-const getTransformBin = (dispatch) => (data, filename) => {
-  const transformed = [];
+// check for the header "GB-BIN01"
+const isBinType = (buffer) => (
+  buffer[0] === 71 && // G
+  buffer[1] === 66 && // B
+  buffer[2] === 45 && // -
+  buffer[3] === 66 && // B
+  buffer[4] === 73 && // I
+  buffer[5] === 78 && // N
+  buffer[6] === 48 && // 0
+  buffer[7] === 49 //    1
+);
+
+const getTransformBin = ({ dispatch }) => async (file) => {
+  const data = await readFileAs(file, 'arrayBuffer');
+  //
+  if (!isBinType(data)) {
+    throw new Error('Not a valid .bin file - is it missing the "GB-BIN01" header?');
+  }
+
+  const tiles = [];
   let currentLine = [];
 
   const binOffsetLength = 8;
 
-  try {
-    for (let i = binOffsetLength; i < data.length; i += 1) {
-      const value = data[i];
-      currentLine.push(value.toString(16).padStart(2, '0'));
+  for (let i = binOffsetLength; i < data.length; i += 1) {
+    const value = data[i];
+    currentLine.push(value.toString(16).padStart(2, '0').toUpperCase());
 
-      if (currentLine.length === 16) {
-        transformed.push(currentLine.join(' '));
-        currentLine = [];
-      }
+    if (currentLine.length === 16) {
+      tiles.push(currentLine.join(' '));
+      currentLine = [];
     }
-
-    // only part of a tile?
-    // -> fill the of it in white
-    if (currentLine.length) {
-      while (currentLine.length < 16) {
-        currentLine.push('00');
-      }
-
-      transformed.push(currentLine.join(' '));
-    }
-
-  } catch (error) {
-    dispatch({
-      type: ERROR,
-      payload: `ERROR_IN_LOADED_BIN\n${filename}`,
-    });
-
-    return Promise.resolve([]);
   }
 
-  return Promise.resolve(transformed);
+  // only part of a tile?
+  // -> fill the rest of it in black
+  if (currentLine.length) {
+    while (currentLine.length < 16) {
+      currentLine.push('FF');
+    }
+
+    tiles.push(currentLine.join(' '));
+  }
+
+  const { dataHash: imageHash } = await compressAndHash(tiles);
+  const { dataHash: frameHash } = await compressAndHashFrame(tiles);
+
+  dispatch({
+    type: IMPORTQUEUE_ADD,
+    payload: {
+      fileName: file.name,
+      imageHash,
+      frameHash,
+      tiles,
+      tempId: Math.random().toString(16).split('.').pop(),
+    },
+  });
 };
 
 export default getTransformBin;
