cb35a21623f9814e8003eb17f980c263b347700e
Andreas Hahn <zatacke@gmail.com>
2023-01-18 13:19:10 +0100

    optimize merge on JSON import

--- src/javascript/tools/mergeStates/index.js
+++ src/javascript/tools/mergeStates/index.js
@@ -1,3 +1,28 @@
+import uniqueBy from '../unique/by';
+
+const mergeBy = (by) => {
+  const unique = uniqueBy(by);
+  return (current, update) => {
+    const mergedUpdate = update.map((item) => {
+      const existingItem = current.find((storeItem) => storeItem[by] === item[by]);
+      if (!existingItem) {
+        return item;
+      }
+
+      return {
+        ...existingItem,
+        ...item,
+      };
+    });
+
+    return unique([...mergedUpdate, ...current]);
+  };
+};
+
+const mergeByHash = mergeBy('hash');
+const mergeById = mergeBy('id');
+const mergeByShortName = mergeBy('shortName');
+
 const mergeStates = (currentState, updatedState, mergeImagesFrames) => {
 
   let frames = currentState.frames;
@@ -6,15 +31,15 @@ const mergeStates = (currentState, updatedState, mergeImagesFrames) => {
 
   if (mergeImagesFrames) {
     if (updatedState.frames && updatedState.frames.length) {
-      frames.unshift(...updatedState.frames);
+      frames = mergeById(frames, updatedState.frames);
     }
 
     if (updatedState.images && updatedState.images.length) {
-      images.unshift(...updatedState.images);
+      images = mergeByHash(images, updatedState.images);
     }
 
     if (updatedState.palettes && updatedState.palettes.length) {
-      palettes.unshift(...updatedState.palettes);
+      palettes = mergeByShortName(palettes, updatedState.palettes);
     }
   } else {
     frames = updatedState.frames || currentState.frames;
