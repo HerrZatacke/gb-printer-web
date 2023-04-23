19a585c2cbeb1f437023bc28ae878fa09052b1b7
Andreas Hahn <zatacke@gmail.com>
2023-04-23 17:01:41 +0200

    Remove stray excalamation mark from classic import/export

--- src/javascript/app/defaults.js
+++ src/javascript/app/defaults.js
@@ -7,10 +7,10 @@ const dateFormatFilename = 'YYYYMMDD-HHmmss';
 
 const defaultGreys = [0x00, 0x55, 0xaa, 0xff];
 
-const initLine = '!{"command":"INIT"}';
-const moreLine = '!{"command":"DATA", "compressed":0, "more":1}';
-const finalLine = '!{"command":"DATA","compressed":0,"more":0}';
-const terminatorLine = '!{"command":"PRNT","sheets":1,"margin_upper":1,"margin_lower":3,"pallet":228,"density":64 }';
+const initLine = '{"command":"INIT"}';
+const moreLine = '{"command":"DATA", "compressed":0, "more":1}';
+const finalLine = '{"command":"DATA","compressed":0,"more":0}';
+const terminatorLine = '{"command":"PRNT","sheets":1,"margin_upper":1,"margin_lower":3,"pallet":228,"density":64 }';
 
 const defaultRGBNPalette = {
   r: defaultGreys.slice(),
