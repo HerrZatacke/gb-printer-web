8c6ecb1218d81c00b839bed0a400168a677ce332
Andreas Hahn <zatacke@gmail.com>
2023-07-14 00:15:03 +0200

    De-duplicate identical banks when importing a ROM and add bank number to image title

--- src/javascript/tools/transformRom/index.js
+++ src/javascript/tools/transformRom/index.js
@@ -2,30 +2,43 @@ import chunk from 'chunk';
 import readFileAs from '../readFileAs';
 import getImportSav from '../transformSav/importSav';
 
-const getTransformSav = ({ getState, dispatch }) => async (file) => {
+const getTransformRom = ({ getState, dispatch }) => async (file) => {
+  const { default: objectHash } = await import(/* webpackChunkName: "obh" */ 'object-hash');
   const data = await readFileAs(file, 'arrayBuffer');
 
   const { importLastSeen, importDeleted, forceMagicCheck } = getState();
 
-  const banks = chunk(data, 0x20000);
+  let banks = chunk(data, 0x20000)
+    .map((bankData, bankIndex) => ({
+      bankData,
+      bankIndex,
+      hash: objectHash(bankData),
+    }));
+
+  banks = banks.filter((bank, index) => (
+    banks.findIndex(({ hash }) => (hash === bank.hash)) === index
+  ));
 
   let displayIndex = 0;
-  const result = await Promise.all(banks.map(async (bankData, bankIndex) => {
+  const result = await Promise.all(banks.map(async ({
+    bankData,
+    bankIndex,
+  }) => {
     if (bankIndex === 0) {
       return false;
     }
 
     const fileName = ({ albumIndex }) => {
       if (albumIndex === 64) {
-        return `${file.name} [last seen bank ${bankIndex}]`;
+        return `${file.name} [last seen in bank ${bankIndex}]`;
       }
 
       if (albumIndex === 255) {
-        return `${file.name} [deleted]`;
+        return `${file.name} [deleted in bank ${bankIndex}]`;
       }
 
       displayIndex += 1;
-      return `${file.name} ${displayIndex.toString(10).padStart(3, '0')}`;
+      return `${file.name} ${displayIndex.toString(10).padStart(3, '0')} in bank ${bankIndex}`;
     };
 
     const importSav = getImportSav({
@@ -52,4 +65,4 @@ const getTransformSav = ({ getState, dispatch }) => async (file) => {
   return result;
 };
 
-export default getTransformSav;
+export default getTransformRom;
