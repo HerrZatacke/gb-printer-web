69a1f765ffc5f05c38103bb762d9e9da4048c762
Andreas Hahn <zatacke@gmail.com>
2024-05-05 20:53:43 +0200

    Refactor TS: plugin middleware

--- src/javascript/app/store/middlewares/plugins.ts
+++ src/javascript/app/store/middlewares/plugins.ts
@@ -7,90 +7,129 @@ import Decoder from '../../../tools/Decoder';
 import { Actions } from '../actions';
 import applyRotation from '../../../tools/applyRotation';
 import { isRGBNImage } from '../../../tools/isRGBNImage';
+import { MiddlewareWithState } from '../../../../types/MiddlewareWithState';
+import { RGBNPalette } from '../../../../types/Image';
+import { Palette } from '../../../../types/Palette';
+import { RGBNTile } from '../../../tools/Decoder/types';
+import { Plugin, PluginArgs, PluginClassInstance, PluginConfigValues, PluginImageData } from '../../../../types/Plugin';
 
-const pluginsMiddleware = (store) => {
-  const registeredPlugins = {};
+interface RegisteredPlugins {
+  [url: string]: PluginClassInstance,
+}
+
+declare global {
+  interface Window {
+    gbpwRegisterPlugin:
+      (PluginClass: { new (config: PluginArgs, stateConfig: PluginConfigValues): PluginClassInstance }) => void;
+  }
+}
+
+const pluginsMiddleware: MiddlewareWithState = (store) => {
+  const registeredPlugins: RegisteredPlugins = {};
   const queue = new Queue(1, Infinity);
 
-  const progress = (progressValue) => {
+  const progress = (progressValue: number): void => {
     store.dispatch({
       type: Actions.EXECUTE_PLUGIN_PROGRESS,
       payload: progressValue % 1,
     });
   };
 
-  const collectImageData = (hash) => {
+  const collectImageData = (hash: string): PluginImageData => {
     const state = store.getState();
     const { handleExportFrame: handleExportFrameState } = state;
+
     const meta = state.images.find((image) => image.hash === hash);
+    if (!meta) {
+      throw new Error('image not found');
+    }
+
     const selectedPalette = getImagePalette(state, meta);
+    if (!selectedPalette) {
+      throw new Error('selectedPalette not found');
+    }
+
     const getTiles = () => loadImageTiles(state)(meta);
 
-    meta.isRGBN = isRGBNImage(meta);
+    const isRGBN = isRGBNImage(meta);
 
-    const getCanvas = ({
+    const getCanvas = async ({
       scaleFactor = 1,
       palette = selectedPalette,
       lockFrame = meta.lockFrame || false,
       invertPalette = meta.invertPalette || false,
       handleExportFrame = handleExportFrameState,
-    } = {}) => (
-      getTiles()
-        .then((tiles) => {
-          const decoder = meta.isRGBN ? new RGBNDecoder() : new Decoder();
+    } = {}): Promise<HTMLCanvasElement> => {
+      const tiles = await getTiles();
+      let decoder: RGBNDecoder | Decoder;
 
-          if (meta.isRGBN) {
-            decoder.update({
-              tiles: RGBNDecoder.rgbnTiles(tiles),
-              palette,
-              lockFrame,
-            });
-          } else {
-            decoder.update({
-              tiles,
-              palette: palette.palette,
-              lockFrame,
-              invertPalette,
-            });
-          }
+      if (isRGBN) {
+        decoder = new RGBNDecoder();
+        decoder.update({
+          canvas: null,
+          tiles: tiles as RGBNTile[],
+          palette: palette as RGBNPalette,
+          lockFrame,
+        });
+      } else {
+        decoder = new Decoder();
+        decoder.update({
+          canvas: null,
+          tiles: tiles as string[],
+          palette: (palette as Palette).palette as string[],
+          lockFrame,
+          invertPalette,
+        });
+      }
 
-          const tempCanvas = decoder.getScaledCanvas(scaleFactor, handleExportFrame);
-          const canvas = document.createElement('canvas');
-          applyRotation(tempCanvas, canvas, meta.rotation);
+      const tempCanvas = decoder.getScaledCanvas(scaleFactor, handleExportFrame);
+      const canvas = document.createElement('canvas');
+      applyRotation(tempCanvas, canvas, meta.rotation || 0);
 
-          return canvas;
-        })
-    );
+      return canvas;
+    };
 
     return {
-      getMeta: () => Promise.resolve(meta),
-      getPalette: () => Promise.resolve(selectedPalette),
+      getMeta: async () => ({ ...meta, isRGBN }),
+      getPalette: async () => (selectedPalette),
       getTiles,
       getCanvas,
     };
   };
 
-  const initPlugin = (plugin) => {
-
-    const pluginState = store.getState().plugins.find(({ url }) => plugin.url === url) || {};
-    const { config: stateConfig = {} } = pluginState;
+  const initPlugin = (plugin: Plugin) => {
+    const pluginState = store.getState().plugins.find(({ url }) => plugin.url === url);
+    const stateConfig = pluginState?.config || {};
     const { url } = plugin;
 
     return (
       queue.add(() => (
         new Promise((resolve) => {
-          window.gbpwRegisterPlugin = (Plugin) => {
+          window.gbpwRegisterPlugin = (PluginClass) => {
             window.gbpwRegisterPlugin = () => { /* noop */ };
 
+            if (!PluginClass) {
+              resolve(false);
+              return;
+            }
+
             try {
-              const instance = new Plugin({
+              const instance: PluginClassInstance = new PluginClass({
                 saveAs,
                 progress,
                 store,
                 collectImageData,
               }, stateConfig);
-              const { name, description = '', configParams = {}, config = {} } = instance;
+
+              const {
+                name,
+                description = '',
+                configParams = {},
+                config = {},
+              } = instance;
+
               registeredPlugins[url] = instance;
+
               store.dispatch({
                 type: Actions.PLUGIN_UPDATE_PROPERTIES,
                 payload: {
@@ -104,13 +143,13 @@ const pluginsMiddleware = (store) => {
                 },
               });
               resolve(true);
-            } catch (error) {
+            } catch (error: unknown) {
               store.dispatch({
                 type: Actions.PLUGIN_UPDATE_PROPERTIES,
                 payload: {
                   url,
                   loading: false,
-                  error: error.message,
+                  error: (error as Error)?.message,
                 },
               });
               resolve(false);
--- src/types/Plugin.ts
+++ src/types/Plugin.ts
@@ -1,16 +1,22 @@
+import { MiddlewareAPI } from 'redux';
+import FileSaver from 'file-saver';
+import { Image, RGBNPalette } from './Image';
+import { Palette } from './Palette';
+import { RGBNTile } from '../javascript/tools/Decoder/types';
+
 enum ConfigParamType {
   NUMBER = 'number',
   STRING = 'string',
   MULTILINE = 'multiline',
 }
 
-interface ConfigParam {
+export interface ConfigParam {
   label: string,
   type: ConfigParamType,
 }
 
 type PluginConfigParams = Record<string, ConfigParam>;
-type PluginConfigValues = Record<string, number | string>;
+export type PluginConfigValues = Record<string, number | string>;
 
 export interface Plugin {
   url: string
@@ -20,4 +26,28 @@ export interface Plugin {
   loading?: boolean
   error?: string | false
   configParams?: PluginConfigParams,
+}
+
+export interface PluginImageData {
+  getMeta: () => Promise<Image & { isRGBN: boolean }>,
+  getPalette: () => Promise<Palette | RGBNPalette>,
+  getTiles: () => Promise<string[] | RGBNTile[] | void>,
+  getCanvas: () => Promise<HTMLCanvasElement>,
+}
+
+export interface PluginClassInstance {
+  name: string,
+  description: string,
+  config: PluginConfigValues,
+  configParams: PluginConfigParams,
+  withImage: (image: PluginImageData) => void,
+  withSelection: (images: PluginImageData[]) => void,
+  setConfig: (config: PluginConfigValues) => void,
+}
+
+export interface PluginArgs {
+  saveAs: typeof FileSaver,
+  progress: (progressValue: number) => void,
+  store: MiddlewareAPI,
+  collectImageData: (hash: string) => PluginImageData,
 }
