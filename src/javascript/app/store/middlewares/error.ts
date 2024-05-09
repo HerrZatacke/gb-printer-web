9af5ee372d27fd39f78fde1b046524133575525c
Andreas Hahn <zatacke@gmail.com>
2024-05-09 07:08:27 +0200

    Refactor TS: ErrorMiddleware

--- src/javascript/app/store/middlewares/error.ts
+++ src/javascript/app/store/middlewares/error.ts
@@ -1,6 +1,7 @@
 import { Actions } from '../actions';
+import { MiddlewareWithState } from '../../../../types/MiddlewareWithState';
 
-const handleErrors = () => (next) => (action) => {
+const handleErrors: MiddlewareWithState = () => (next) => (action) => {
 
   if (action.type === Actions.ERROR) {
     // eslint-disable-next-line no-alert
