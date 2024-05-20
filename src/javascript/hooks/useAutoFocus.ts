1af6e8d8220497b455fa1a9f1b7c5c5067dd9867
Andreas Hahn <zatacke@gmail.com>
2024-05-20 16:21:18 +0200

    Refactor TS: useAutoFocus hook

--- src/javascript/hooks/useAutoFocus.ts
+++ src/javascript/hooks/useAutoFocus.ts
@@ -2,20 +2,20 @@ import { useEffect, useRef } from 'react';
 import isTouchDevice from '../tools/isTouchDevice';
 
 const useAutoFocus = () => {
-  const ref = useRef();
+  const ref = useRef<Element>();
   useEffect(() => {
-    const lastFocussedElement = document.activeElement;
+    const lastFocussedElement = document.activeElement as HTMLElement | null;
 
-    const focusables = [
+    const focusables = ref.current ? [
       ...ref.current.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'),
-    ];
+    ] : [];
 
     if (focusables.length && !isTouchDevice()) {
-      focusables[0].focus();
+      (focusables[0] as HTMLElement).focus();
     }
 
     return () => {
-      lastFocussedElement.focus();
+      lastFocussedElement?.focus();
     };
   }, []);
 
