29ed35786c5fa59a46d49699ef697d519b9b3da1
Andreas Hahn <zatacke@gmail.com>
2024-05-27 02:24:45 +0200

    Refactor to async for index.ts

--- src/javascript/index.ts
+++ src/javascript/index.ts
@@ -3,7 +3,7 @@ import isTouchDevice from './tools/isTouchDevice';
 import { loadEnv } from './tools/getEnv';
 import initLog from './tools/initLog';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 
   // set the theme class as quick as possible
   const theme = localStorage.getItem('gbp-web-theme');
@@ -19,23 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   initLog('Loading environment information');
 
-  loadEnv()
-    .then(() => {
+  try {
+    await loadEnv();
+    initLog('Loading main app file');
+    const { default: initApp } = await import(/* webpackChunkName: "iap" */'./app/initApp');
+    initLog('Starting app');
+    await initApp();
+  } catch (error) {
+    const appNode = document.getElementById('app');
 
-      initLog('Loading main app file');
-
-      return import(/* webpackChunkName: "iap" */'./app/initApp')
-        .then(({ default: initApp }) => {
-          initLog('Starting app');
-          initApp();
-        });
-    })
-    .catch((error) => {
-      const appNode = document.getElementById('app');
-
-      if (appNode) {
-        appNode.innerHTML =
-          `<p class="init-error">This browser is not supported.<br/>This App is being optimized for chromium-based browsers.<small>${error.stack}</small></p>`;
-      }
-    });
+    if (appNode) {
+      appNode.innerHTML =
+        `<p class="init-error">This browser is not supported.<br/>This App is being optimized for chromium-based browsers.<small>${(error as Error).stack}</small></p>`;
+    }
+  }
 });
