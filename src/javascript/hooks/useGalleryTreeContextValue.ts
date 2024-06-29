e89c7a98d780aac8d1e193b75eb9b5678e87e5fe
Andreas Hahn <zatacke@gmail.com>
2024-06-29 16:16:09 +0200

    Add counter for duplicate slugs

--- src/javascript/hooks/useGalleryTreeContextValue.ts
+++ src/javascript/hooks/useGalleryTreeContextValue.ts
@@ -52,7 +52,14 @@ const ensureSingleUsage = (groups: SerializableImageGroup[]): SingleUsageResult 
 
 const reducePaths = (prefix: string, groups: TreeImageGroup[]): PathMap => (
   groups.reduce((acc: PathMap, group: TreeImageGroup): PathMap => {
-    const absolute = `${prefix}${group.slug}/`;
+    let count = 0;
+    let absolute = `${prefix}${group.slug}/`;
+
+    while (acc[absolute]) {
+      count += 1;
+      absolute = `${prefix}${group.slug}_${count}/`;
+    }
+
     return ({
       ...acc,
       [absolute]: group,
@@ -157,7 +164,11 @@ export const useGalleryTreeContextValue = (): GalleryTreeContext => {
     return newRoot;
   }, [singleUsageResult, stateImages, dispatch]);
 
-  const paths = useMemo<PathMap>((): PathMap => reducePaths('', root.groups), [root]);
+  const paths = useMemo<PathMap>((): PathMap => {
+    const p = reducePaths('', root.groups);
+    console.log(p);
+    return p;
+  }, [root]);
 
   const result = useMemo<GalleryTreeContext>((): GalleryTreeContext => {
     const view = paths[path] || root;
