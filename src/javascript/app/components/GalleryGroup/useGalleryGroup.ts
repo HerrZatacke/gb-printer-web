73fcec12ee26330e84732c7dd4ce705a792398bb
Andreas Hahn <zatacke@gmail.com>
2024-06-29 17:15:44 +0200

    Fixed imagegroups with same cover image generating wrong links to childgroup

--- src/javascript/app/components/GalleryGroup/useGalleryGroup.ts
+++ src/javascript/app/components/GalleryGroup/useGalleryGroup.ts
@@ -1,5 +1,6 @@
 import type { TreeImageGroup } from '../../../../types/ImageGroup';
 import { useGalleryTreeContext } from '../../contexts/galleryTree';
+import { useGalleryParams } from '../../../hooks/useGalleryParams';
 
 interface UseGalleryGroup {
   group: TreeImageGroup | null,
@@ -8,6 +9,7 @@ interface UseGalleryGroup {
 
 export const useGalleryGroup = (hash: string): UseGalleryGroup => {
   const { paths } = useGalleryTreeContext();
+  const { path: viewPath } = useGalleryParams();
 
   const path = Object.keys(paths)
     .reduce((acc: string | null, reducePath: string): string | null => {
@@ -15,7 +17,10 @@ export const useGalleryGroup = (hash: string): UseGalleryGroup => {
         return acc;
       }
 
-      if (paths[reducePath].coverImage === hash) {
+      // if two groups use the same coverimage, this may
+      // cause the wrong link to be generated. Maybe a ToDo?
+      // depending on order of creation of the groups
+      if (paths[reducePath].coverImage === hash && reducePath !== viewPath) {
         return reducePath;
       }
 
