fb6c74c61e4312254c68d8728e0e5a6444147834
Andreas Hahn <zatacke@gmail.com>
2025-02-23 01:15:03 +0100

    Improved user feedback for storage persistance request

--- src/javascript/app/components/Settings/pages/ExportSettings/useStoragePersist.ts
+++ src/javascript/app/components/Settings/pages/ExportSettings/useStoragePersist.ts
@@ -1,24 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+export enum PersistState {
+  PERSISTED = 'persisted',
+  NOT_PERSISTED = 'not-persisted',
+  FAILED = 'failed',
+  NO_API = 'no_api',
+}
 
 interface UseStoragePersist {
   persistAPIAvailable: boolean,
-  persisted: boolean,
+  persisted: PersistState,
   requestPersist: () => void,
 }
 
-const useStoragePersist = (): UseStoragePersist => {
-  const [persisted, setPersisted] = useState(false);
-  const persistAPIAvailable = !!(navigator.storage && navigator.storage.persist);
+const persistAPIAvailable = !!(navigator.storage && navigator.storage.persist);
 
+const getPersistState = async (set: (state: PersistState) => void) => {
   if (persistAPIAvailable) {
-    navigator.storage.persisted().then(setPersisted);
+    const success = await navigator.storage.persisted();
+    set(success ? PersistState.PERSISTED : PersistState.NOT_PERSISTED);
+  } else {
+    set(PersistState.NO_API);
   }
+};
 
-  const requestPersist = () => {
-    setPersisted(false);
-    if (persistAPIAvailable) {
-      navigator.storage.persist().then(setPersisted);
-    }
+const setPersistState = async (set: (state: PersistState) => void) => {
+  if (persistAPIAvailable) {
+    const success = await navigator.storage.persist();
+    set(success ? PersistState.PERSISTED : PersistState.FAILED);
+  } else {
+    set(PersistState.NO_API);
+  }
+};
+
+const useStoragePersist = (): UseStoragePersist => {
+  const [persisted, setPersisted] = useState(PersistState.NOT_PERSISTED);
+
+  useEffect(() => {
+    getPersistState(setPersisted);
+  }, []);
+
+  const requestPersist = async () => {
+    setPersistState(setPersisted);
   };
 
   return {
