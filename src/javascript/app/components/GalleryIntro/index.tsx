9beee5900b90b569536fb98ff77167cac28156ea
Andreas Hahn <zatacke@gmail.com>
2025-02-23 02:47:41 +0100

    Added storage usage information

--- src/javascript/app/components/GalleryIntro/index.tsx
+++ src/javascript/app/components/GalleryIntro/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import bytes from 'bytes';
+import type { CSSPropertiesVars } from 'react';
+import { useStorageInfo } from './useStorageInfo';
 import './index.scss';
 
 interface Props {
@@ -7,7 +10,19 @@ interface Props {
   filteredCount: number,
 }
 
+const storageLabel = (key: string) => {
+  switch (key) {
+    case 'indexedDB':
+      return 'indexed DB';
+    case 'localStorage':
+      return 'local storage';
+    default:
+      return key;
+  }
+};
+
 function GalleryIntroText(props: Props) {
+  const { storageEstimate } = useStorageInfo();
   return (
     <>
       <h2 className="gallery-intro__counter">
@@ -21,6 +36,20 @@ function GalleryIntroText(props: Props) {
         <br />
         Also if you clear your browser&apos;s cookies, the images will be gone too.
       </p>
+      {
+        storageEstimate && (
+          <p
+            key={storageEstimate.type}
+            className="gallery-intro__storage-limit"
+            style={{
+              '--percentage': `${storageEstimate.percentage}%`,
+            } as CSSPropertiesVars}
+            title={`Using ${bytes(storageEstimate.used)} of ${bytes(storageEstimate.total)}`}
+          >
+            { `You are using ${storageEstimate.percentage}% of your browser's ${storageLabel(storageEstimate.type)}. Be aware for now saving images will not be able once you hit the limit.` }
+          </p>
+        )
+      }
     </>
   );
 }
