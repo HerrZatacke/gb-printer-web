5a6ba23abc7160119d7679ac9b32454d7e4c6c97
Andreas Hahn <zatacke@gmail.com>
2025-04-12 12:19:58 +0200

    Refactor WebUSB Greeting page and created AsyncMarkdown wrapper Component

--- src/javascript/app/components/Home/index.tsx
+++ src/javascript/app/components/Home/index.tsx
@@ -1,46 +1,16 @@
-import type { FC, PropsWithChildren } from 'react';
-import React, { useEffect, useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Stack from '@mui/material/Stack';
-import type { MuiMarkdownProps } from 'mui-markdown';
-
-function GappedStack({ children }: PropsWithChildren) {
-  return (
-    <Stack
-      direction="column"
-      gap={4}
-    >
-      {children}
-    </Stack>
-  );
-}
+import React from 'react';
+import AsyncMarkdown from '../AsyncMarkdown';
 
 function Home() {
-  const [MuiMarkdown, setMuiMarkdown] = useState<FC<MuiMarkdownProps>>(() => () => null);
-  const [docs, setDocs] = useState<string>('');
-
-  useEffect(() => {
-    const load = async () => {
-      const { default: MMD } = await import(/* webpackChunkName: "mmd" */ 'mui-markdown');
-      setMuiMarkdown(() => MMD);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const { default: rawMd } = await import(/* webpackChunkName: "doc" */ '../../../../../README.md');
-      setDocs(rawMd);
-    };
-
-    load();
-  }, []);
-
   return (
-    <Card className="home">
-      <CardContent>
-        <MuiMarkdown options={{ wrapper: GappedStack }}>
-          {docs}
-        </MuiMarkdown>
-      </CardContent>
-    </Card>
+    <AsyncMarkdown
+      getMarkdown={async () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const { default: rawMd } = await import(/* webpackChunkName: "doc" */ '../../../../../README.md');
+        return rawMd;
+      }}
+    />
   );
 }
 
---
+++ src/javascript/app/components/WebUSBGreeting/WebUSB.md
@@ -0,0 +1,8 @@
+## Using a WebUSB or WebSerial device:
+After connecting a WebUSB or WebSerial device, you can continue to the [Gallery Page](#/gallery) or [Home Page](#/home) now.  
+An overlay will appear once you're receiving data.  
+You can also access these options via the USB-Symbol in the navigation and the [Settings Page](#/settings).  
+
+## Note: WebUSB and Web Serial are similar but not the same:
+* While Web Serial has access to a regular COM-port and does not require special devices, the feature is not available on mobile devices.
+* WebUSB requires certain microcontrollers where the processor has direct access to the USB interface (e.g. an Arduino Leonardo). See [webusb for arduino on GitHub](https://github.com/webusb/arduino) for more information.
