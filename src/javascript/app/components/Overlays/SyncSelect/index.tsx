6140d597ed0b02fb013cd206eab618c8ee5df701
Andreas Hahn <zatacke@gmail.com>
2025-04-12 13:30:20 +0200

    Refactored Sync Overlay for MUI

--- src/javascript/app/components/Overlays/SyncSelect/index.tsx
+++ src/javascript/app/components/Overlays/SyncSelect/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import classnames from 'classnames';
-import OldLightbox from '../../Lightbox';
-import SVG from '../../SVG';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import Lightbox from '../../Lightbox';
 import { useSyncSelect } from './useSyncSelect';
 
-import './index.scss';
+const getButtonColor = (showSyncHints: boolean, warn: boolean): 'secondary' | 'error' => {
+  if (!showSyncHints) {
+    return 'secondary';
+  }
+
+  return warn ? 'error' : 'secondary';
+};
 
 function SyncSelect() {
   const {
@@ -20,123 +30,111 @@ function SyncSelect() {
   const showSyncHints = autoDropboxSync && (syncLastUpdate.local !== syncLastUpdate.dropbox);
 
   return (
-    <OldLightbox
-      className="sync-select"
+    <Lightbox
       deny={cancelSync}
       header="Synchronize"
     >
-      <ul className="sync-select__list">
+      <Stack
+        direction="column"
+        gap={4}
+        sx={{ '& .MuiButton-endIcon svg': { fontSize: 32 } }}
+      >
         {gitActive && (
-          <>
-            <li className="sync-select__option">
-              <button
-                type="button"
-                className="sync-select__button button"
-                title={`Synchronize to GitHub\n${repoUrl}`}
-                onClick={() => {
-                  startSync('git', 'up');
-                }}
-              >
-                <span className="sync-select__button-label">
-                  Synchronize to GitHub
-                </span>
-                <SVG name="sync" className="svg--180" />
-              </button>
-            </li>
-            <li className="sync-select__option">
-              <button
-                type="button"
-                className="sync-select__button button"
-                title={`Synchronize from GitHub\n${repoUrl}`}
-                onClick={() => {
-                  startSync('git', 'down');
-                }}
-              >
-                <span className="sync-select__button-label">
-                  Synchronize from GitHub
-                </span>
-                <SVG name="sync" />
-              </button>
-            </li>
-          </>
+          <Stack
+            direction="column"
+            gap={2}
+          >
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<GitHubIcon />}
+              endIcon={<CloudUploadIcon />}
+              title={`Synchronize to GitHub\n${repoUrl}`}
+              onClick={() => {
+                startSync('git', 'up');
+              }}
+            >
+              Synchronize to GitHub
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<GitHubIcon />}
+              endIcon={<CloudDownloadIcon />}
+              title={`Synchronize from GitHub\n${repoUrl}`}
+              onClick={() => {
+                startSync('git', 'down');
+              }}
+            >
+              Synchronize from GitHub
+            </Button>
+          </Stack>
         )}
         {dropboxActive && (
-          <>
-            <li className="sync-select__option">
-              <button
-                type="button"
-                className="sync-select__button button"
+          <Stack
+            direction="column"
+            gap={2}
+          >
+            <Stack
+              direction="column"
+              gap={1}
+            >
+              <Button
+                variant="contained"
+                color={getButtonColor(showSyncHints, syncLastUpdate.local < syncLastUpdate.dropbox)}
+                endIcon={<CloudUploadIcon />}
                 title="Synchronize to Dropbox"
                 onClick={() => {
                   startSync('dropbox', 'up');
                 }}
               >
-                <span className="sync-select__button-label">
-                  Synchronize to Dropbox
-                </span>
-                <SVG name="sync" className="svg--180" />
-                {showSyncHints && (
-                  <span
-                    className={classnames('sync-select__button-hint', {
-                      'sync-select__button-hint--warn': syncLastUpdate.local < syncLastUpdate.dropbox,
-                    })}
-                  >
-                    {
-                      syncLastUpdate.local < syncLastUpdate.dropbox ?
-                        'There are pending remote changes. If you synchronize to dropbox now, these will be lost.' :
-                        'Upload changes to dropbox'
-                    }
-                  </span>
-                )}
-              </button>
-            </li>
-            <li className="sync-select__option">
-              <button
-                type="button"
-                className="sync-select__button button"
+                Synchronize to Dropbox
+              </Button>
+              {showSyncHints && syncLastUpdate.local < syncLastUpdate.dropbox && (
+                <Typography variant="caption">
+                  There are pending remote changes. If you synchronize to dropbox now, these will be lost.
+                </Typography>
+              )}
+            </Stack>
+
+            <Stack
+              direction="column"
+              gap={1}
+            >
+              <Button
+                variant="contained"
+                color={getButtonColor(showSyncHints, syncLastUpdate.local > syncLastUpdate.dropbox)}
+                endIcon={<CloudDownloadIcon />}
                 title="Synchronize from Dropbox"
                 onClick={() => {
                   startSync('dropbox', 'down');
                 }}
               >
-                <span className="sync-select__button-label">
-                  Synchronize from Dropbox
-                </span>
-                <SVG name="sync" />
-                {showSyncHints && (
-                  <span
-                    className={classnames('sync-select__button-hint', {
-                      'sync-select__button-hint--warn': syncLastUpdate.local > syncLastUpdate.dropbox,
-                    })}
-                  >
-                    {
-                      syncLastUpdate.local > syncLastUpdate.dropbox ?
-                        'There are local changes not synched to dropbox yet. If you synchronize from dropbox now, these will be lost.' :
-                        'Download changes from dropbox'
-                    }
-                  </span>
-                )}
-              </button>
-            </li>
-            <li className="sync-select__option">
-              <button
-                type="button"
-                className="sync-select__button button"
-                title="Synchronize images to Dropbox"
-                onClick={() => {
-                  startSync('dropboximages', 'up');
-                }}
-              >
-                <span className="sync-select__button-label">
-                  Synchronize images to Dropbox
-                </span>
-                <SVG name="sync" className="svg--180" />
-              </button>
-            </li>
-          </>
+                Synchronize from Dropbox
+              </Button>
+              {showSyncHints && syncLastUpdate.local > syncLastUpdate.dropbox && (
+                <Typography variant="caption">
+                  There are local changes not synched to dropbox yet.
+                  If you synchronize from dropbox now, these will be lost.
+                </Typography>
+              )}
+            </Stack>
+
+            <Button
+              variant="contained"
+              color="secondary"
+              endIcon={<CloudUploadIcon />}
+              title="Synchronize images to Dropbox"
+              onClick={() => {
+                startSync('dropboximages', 'up');
+              }}
+            >
+              Synchronize images to Dropbox
+            </Button>
+          </Stack>
         )}
-      </ul>
-    </OldLightbox>
+      </Stack>
+    </Lightbox>
   );
 }
 
