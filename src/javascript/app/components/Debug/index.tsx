dc4fc7846569529bba2e3593234cdc68c1451184
Andreas Hahn <zatacke@gmail.com>
2025-04-14 22:30:57 +0200

    Refactor Debug-component for MUI

--- src/javascript/app/components/Debug/index.tsx
+++ src/javascript/app/components/Debug/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import type { Theme } from '@mui/system';
 import useSettingsStore from '../../stores/settingsStore';
-import './index.scss';
 
 interface Props {
   children: React.ReactNode,
@@ -15,9 +16,23 @@ function Debug({ children }: Props) {
   }
 
   return (
-    <pre className="debug">
+    <Typography
+      variant="caption"
+      fontFamily="monospace"
+      sx={(theme: Theme) => ({
+        wordBreak: 'break-all',
+        whiteSpace: 'pre-wrap',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.warning[theme.palette.mode],
+        borderRadius: '3px',
+        px: 0.5,
+        mx: -0.5,
+        mt: 1,
+        mb: 0,
+      })}
+    >
       { children }
-    </pre>
+    </Typography>
   );
 }
 
