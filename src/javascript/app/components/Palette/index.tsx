41b4d7816edc96cb830729f70a717456da1e9d91
Andreas Hahn <zatacke@gmail.com>
2025-04-18 15:32:29 +0200

    Refactored Palettes to use MUI

--- src/javascript/app/components/Palette/index.tsx
+++ src/javascript/app/components/Palette/index.tsx
@@ -1,13 +1,11 @@
-import type { CSSPropertiesVars } from 'react';
-import React from 'react';
-
-import classnames from 'classnames';
-import SVG from '../SVG';
+import React, { useMemo } from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { usePalette } from './usePalette';
-
-import './index.scss';
-
-const GRADIENT_OPACITY = '36';
+import GalleryGridItem from '../GalleryGridItem';
+import PaletteContextMenu from '../PaletteContextMenu';
+import { hexToRgbString } from '../../../tools/hexToRgbString';
 
 interface Props {
   shortName: string,
@@ -17,14 +15,13 @@ interface Props {
   usage: number,
 }
 
-function Component({
+function Palette({
   shortName,
   name,
   isPredefined,
   palette,
   usage,
 }: Props) {
-
   const {
     isActive,
     setActive,
@@ -33,78 +30,66 @@ function Component({
     clonePalette,
   } = usePalette(shortName, name);
 
+  const theme = useTheme();
+
+  const paletteStyles = useMemo(() => {
+    const styles = {
+      '--gradient-1': `${hexToRgbString(palette[0])}`,
+      '--gradient-2': `${hexToRgbString(palette[1])}`,
+      '--gradient-3': `${hexToRgbString(palette[2])}`,
+      '--gradient-4': `${hexToRgbString(palette[3])}`,
+      '--gradient-opacity': '0.25',
+      backgroundImage: 'linear-gradient(to right, rgba(var(--gradient-1), var(--gradient-opacity)) 0%, rgba(var(--gradient-2), var(--gradient-opacity)) 33.3%, rgba(var(--gradient-3), var(--gradient-opacity)) 66.6%, rgba(var(--gradient-4), var(--gradient-opacity)) 100%)',
+      display: 'block',
+      width: '100%',
+      textAlign: 'left',
+      '.MuiTypography-body1': {
+        fontWeight: isActive ? 'bold' : 'normal',
+      },
+    };
+
+    return styles;
+  }, [palette, isActive]);
+
+  const mediaStyles = useMemo(() => {
+    const styles = {
+      backgroundImage: 'linear-gradient(to right, rgb(var(--gradient-1)) 0%, rgb(var(--gradient-1)) 25%, rgb(var(--gradient-2)) 25%, rgb(var(--gradient-2)) 50%, rgb(var(--gradient-3)) 50%, rgb(var(--gradient-3)) 75%, rgb(var(--gradient-4)) 75%, rgb(var(--gradient-4)) 100%)',
+      display: 'block',
+      width: '100%',
+      height: theme.spacing(8),
+    };
+
+    return styles;
+  }, [theme]);
+
   if (palette?.length !== 4) {
     return <li className="palette palette--broken" />;
   }
 
   return (
-    <li
-      className={
-        classnames('palette', {
-          'palette--active': isActive,
-        })
-      }
-      style={{
-        '--gradient-1': `${palette[0]}${GRADIENT_OPACITY}`,
-        '--gradient-2': `${palette[1]}${GRADIENT_OPACITY}`,
-        '--gradient-3': `${palette[2]}${GRADIENT_OPACITY}`,
-        '--gradient-4': `${palette[3]}${GRADIENT_OPACITY}`,
-      } as CSSPropertiesVars}
-    >
-      <button
-        onClick={() => {
-          setActive();
-        }}
-        className="palette__button"
-        type="button"
-      >
-        <div className="palette__name">
-          {name}
-          <span className="palette__shortname">{shortName}</span>
-          <span className="palette__usage">{usage ? `Used ${usage} times` : 'Not used'}</span>
-        </div>
-        <div className="palette__colors">
-          <div className="palette__color" style={{ backgroundColor: palette[0] }} title={palette[0]}> </div>
-          <div className="palette__color" style={{ backgroundColor: palette[1] }} title={palette[1]}> </div>
-          <div className="palette__color" style={{ backgroundColor: palette[2] }} title={palette[2]}> </div>
-          <div className="palette__color" style={{ backgroundColor: palette[3] }} title={palette[3]}> </div>
-        </div>
-      </button>
-      <div className="palette__manage-buttons">
-        <button
-          type="button"
-          title="Clone palette"
-          className="palette__manage-button palette__manage-button--clone"
-          onClick={() => clonePalette()}
-        >
-          <SVG name="clone" />
-        </button>
-        {
-          isPredefined ? null : (
-            <>
-              <button
-                type="button"
-                title="Edit palette"
-                className="palette__manage-button palette__manage-button--edit"
-                onClick={() => editPalette()}
-              >
-                <SVG name="edit" />
-              </button>
-              <button
-                type="button"
-                title="Delete palette"
-                className="palette__manage-button palette__manage-button--delete"
-                onClick={() => deletePalette()}
-              >
-                <SVG name="delete" />
-              </button>
-            </>
-          )
-        }
-      </div>
-    </li>
+    <GalleryGridItem
+      selectionText=""
+      title={name}
+      subheader={`(${shortName}) ${usage ? `Used ${usage} times` : 'Not used'}`}
+      titleIcon={isActive ? <CheckCircleIcon /> : undefined}
+      wrapperProps={{
+        sx: paletteStyles,
+        onClick: editPalette,
+        disableRipple: true,
+      }}
+      contextMenuComponent={PaletteContextMenu}
+      contextMenuProps={{
+        isPredefined,
+        setActive,
+        clonePalette,
+        deletePalette,
+        editPalette,
+      }}
+      media={<Box sx={mediaStyles} />}
+      content={null}
+    />
   );
 }
 
 
-export default Component;
+export default Palette;
--- src/javascript/consts/GalleryViews.ts
+++ src/javascript/consts/GalleryViews.ts
@@ -3,4 +3,5 @@ export enum GalleryViews {
   GALLERY_VIEW_1X = '1x',
   GALLERY_VIEW_2X = '2x',
   GALLERY_VIEW_MAX = 'max',
+  PALETTE_VIEW = 'res2x',
 }
