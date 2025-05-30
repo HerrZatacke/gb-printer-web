cd8fca1c742d98cb14e1a4d0d4d64174d3d4566d
Andreas Hahn <zatacke@gmail.com>
2025-05-30 18:49:25 +0200

    Move files for NextJS migration

--- src/javascript/db.ts
+++
@@ -1,6 +0,0 @@
-import { initDbTransfer } from './database';
-import '../scss/index.scss';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  await initDbTransfer(window.opener);
-});
