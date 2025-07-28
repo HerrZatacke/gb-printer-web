e68d967645c67b679df141a2ff28477b824fcf27
Andreas Hahn <zatacke@gmail.com>
2025-07-28 23:15:02 +0200

    Added specific README files for project's published startpage

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,36 @@
+'use client';
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import  MuiMarkdown from 'mui-markdown';
+import { useEffect, useState } from 'react';
 import MarkdownStack from '@/components/MarkdownStack';
-import readme from '../../../README.md';
+import { shortLocales } from '@/i18n/locales';
+import readmeEn from '@/i18n/markdown/Startpage/en.md';
+import useSettingsStore from '@/stores/settingsStore';
 
 export default function Home() {
+  const [readme, setReadme] = useState(readmeEn);
+  const { preferredLocale } = useSettingsStore();
+
+  useEffect(() => {
+    const set = async () => {
+      let langFile = preferredLocale.split('-')[0];
+
+      if (!shortLocales.includes(langFile)) {
+        langFile = 'en';
+      }
+
+      try {
+        setReadme((await import(`@/i18n/markdown/Startpage/${langFile}.md`)).default);
+      } catch {
+        setReadme(readmeEn);
+      }
+    };
+
+    set();
+  }, [preferredLocale]);
+
   return (
     <Card>
       <CardContent>
