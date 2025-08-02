5730b55e81cb16ed7d6a30301b6480cbc6e4a89a
Andreas Hahn <zatacke@gmail.com>
2025-08-02 23:46:53 +0200

    Added hover for outlined MUI-button

--- src/styles/components/button.ts
+++ src/styles/components/button.ts
@@ -1,3 +1,4 @@
+import { alpha } from '@mui/material';
 import type { Components } from '@mui/material/styles';
 import type { Theme } from '@mui/system';
 
@@ -18,9 +19,15 @@ export const button = (theme: Theme): Components['MuiButton'] => ({
           // background: theme.palette[color].main,
           color: theme.palette[color].light,
           borderColor: theme.palette[color].light,
-          // '&:hover': {
-          //   backgroundColor: alpha(theme.palette[color].main, 0.7),
-          // },
+          '&:hover': {
+            backgroundColor: alpha(theme.palette[color].main, 0.25),
+          },
+          '&.Mui-disabled': {
+            backgroundColor: alpha(theme.palette.fgtext.main, 0.33),
+            color: theme.palette.fgtext.main,
+            borderColor: theme.palette.fgtext.main,
+            opacity: 0.4,
+          },
         },
         // '&:hover': {
         //   backgroundColor: alpha(theme.palette[color].main, 0.4),
