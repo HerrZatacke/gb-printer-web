e50a9577d9d02d0357592e07627616d4d3424abb
Andreas Hahn <zatacke@gmail.com>
2025-10-08 18:39:25 +0200

    Removed unnecessary aspect ratio

--- src/components/Overlays/LightboxImages/LightBoxImage.tsx
+++ src/components/Overlays/LightboxImages/LightBoxImage.tsx
@@ -2,14 +2,12 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import React from 'react';
 import ImageRender from '@/components/ImageRender';
-import { useImageDimensions } from '@/hooks/useImageDimensions';
 
 export interface LightBoxImageProps {
   hash: string,
 }
 
 function LightBoxImage({ hash }: LightBoxImageProps) {
-  const { dimensions } = useImageDimensions(hash);
   return (
     <Stack
       direction="column"
@@ -21,12 +19,7 @@ function LightBoxImage({ hash }: LightBoxImageProps) {
       }}
       justifyContent="center"
     >
-      <Box
-        sx={{
-          aspectRatio: dimensions.aspectRatioCSS,
-          maxHeight: '100%',
-        }}
-      >
+      <Box sx={{ maxHeight: '100%' }}>
         <ImageRender hash={hash}/>
       </Box>
     </Stack>
