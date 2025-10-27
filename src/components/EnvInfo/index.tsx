1f49486192743efcd7404bd6bf31abfb9eca200e
Andreas Hahn <zatacke@gmail.com>
2025-10-27 01:16:32 +0100

    Added reporting of memory usage to env info

--- src/components/EnvInfo/index.tsx
+++ src/components/EnvInfo/index.tsx
@@ -2,13 +2,16 @@
 
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import filesize from 'filesize';
 import { useTranslations } from 'next-intl';
 import { useMemo } from 'react';
 import { useEnv } from '@/contexts/envContext';
+import { useStorageInfo } from '@/hooks/useStorageInfo';
 
 function EnvInfo() {
   const env = useEnv();
   const t = useTranslations('EnvInfo');
+  const { storageEstimate } = useStorageInfo();
 
   const infos = useMemo<string[]>(() => {
     if (!env) {
@@ -21,12 +24,20 @@ function EnvInfo() {
       t('printerVersion', { value: env.version }),
       t('maxImages', { value: env.maximages }),
       t('storageDriver', { value: env.localforage }),
+      ...storageEstimate.map((estimate) => (
+        t('storageEstimate', {
+          type: estimate.type,
+          total: filesize(estimate.total),
+          used: filesize(estimate.used),
+          percentage: estimate.percentage,
+        })
+      )),
       t('envType', { value: env.env }),
       t('filesystem', { value: env.fstype }),
       t('bootmode', { value: env.bootmode }),
       t('hasOled', { value: env.oled ? t('yes') : t('no') }),
     ]);
-  }, [env, t]);
+  }, [env, storageEstimate, t]);
 
   return (
     <Stack
