8c09b5d1dbc124f751748d6d31f777955de1ef19
Andreas Hahn <zatacke@gmail.com>
2026-02-07 13:52:14 +0100

    Refactored GisContext pattern

---
+++ src/contexts/GisContext/hook.ts
@@ -0,0 +1,113 @@
+import { useEffect, useCallback, useState, useMemo } from 'react';
+import { useLoadScript } from '@/hooks/useLoadScript';
+import { useStoragesStore } from '@/stores/stores';
+import TokenClient = google.accounts.oauth2.TokenClient;
+
+export interface GISContextType {
+  isSignedIn: boolean;
+  handleSignIn: () => Promise<void>;
+  handleSignOut: () => Promise<void>;
+}
+
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID || '';
+const scope = process.env.NEXT_PUBLIC_GOOGLE_SCOPE || '';
+
+export const useContextHook = (): GISContextType => {
+  const { loadScript } = useLoadScript();
+  const { setGapiSettings, gapiStorage } = useStoragesStore();
+  const [tokenClient, setTokenClient] = useState<TokenClient | null>(null);
+
+  const isSignedIn = useMemo(() => (
+    Boolean(gapiStorage.token)
+  ), [gapiStorage.token]);
+
+  const createTokenClient = useCallback(() => {
+    if (!clientId || !scope) {
+      return;
+    }
+
+    setTokenClient((currentTokenClient) => {
+      if (currentTokenClient) {
+        return currentTokenClient;
+      }
+
+      return google.accounts.oauth2.initTokenClient({
+        client_id: clientId,
+        scope,
+        callback: (tokenResponse) => {
+          console.log('🤖 callback');
+          if (tokenResponse.error) {
+            throw new Error(tokenResponse.error_description);
+          }
+
+          setGapiSettings({
+            token: tokenResponse.access_token,
+            tokenExpiry: Date.now() + (parseInt(tokenResponse.expires_in || '0', 10) * 1000),
+          });
+        },
+      });
+    });
+  }, [setGapiSettings]);
+
+  useEffect(() => {
+    const { use, tokenExpiry, token } = gapiStorage;
+    if (!clientId || !use || !token) {
+      return;
+    }
+
+    const tokenExpiresInMs = (tokenExpiry || 0) - Date.now();
+
+    const tokenExpiryTimerHandle = window.setTimeout(() => {
+      if (!tokenClient) {
+        return;
+      }
+
+      console.log('🤖 Token expired!');
+      tokenClient.requestAccessToken({ prompt: '' });
+      setGapiSettings({
+        token: '',
+        tokenExpiry: 0,
+      });
+    }, tokenExpiresInMs);
+
+    return () => window.clearTimeout(tokenExpiryTimerHandle);
+  }, [gapiStorage, setGapiSettings, tokenClient]);
+
+  useEffect(() => {
+    if (!clientId || !gapiStorage.use) {
+      return;
+    }
+
+    const handle = setTimeout(async () => {
+      await loadScript('https://accounts.google.com/gsi/client');
+      await loadScript('https://apis.google.com/js/api.js');
+      await createTokenClient();
+    }, 1);
+
+    return () => clearTimeout(handle);
+  }, [gapiStorage.use, createTokenClient, loadScript]);
+
+  const handleSignIn = useCallback(async () => {
+    if (!tokenClient) {
+      console.log('no tokenClient');
+      return;
+    }
+
+    tokenClient.requestAccessToken({ prompt: '' });
+  }, [tokenClient]);
+
+  const handleSignOut = useCallback(async () => {
+    if (!gapiStorage.token) {
+      return;
+    }
+
+    google.accounts.oauth2.revoke(gapiStorage?.token, () => {
+      setGapiSettings({
+        token: '',
+        tokenExpiry: 0,
+      });
+    });
+  }, [gapiStorage.token, setGapiSettings]);
+
+  return { handleSignIn, handleSignOut, isSignedIn };
+};
--- src/contexts/GisContext/index.tsx
+++ src/contexts/GisContext/index.tsx
@@ -1,15 +1,7 @@
 'use client';
 
-import { type PropsWithChildren, createContext, useEffect, useCallback, useState, useMemo, useContext } from 'react';
-import { useLoadScript } from '@/hooks/useLoadScript';
-import { useStoragesStore } from '@/stores/stores';
-import TokenClient = google.accounts.oauth2.TokenClient;
-
-interface GISContextType {
-  isSignedIn: boolean;
-  handleSignIn: () => Promise<void>;
-  handleSignOut: () => Promise<void>;
-}
+import { type PropsWithChildren, createContext, useContext } from 'react';
+import { GISContextType, useContextHook } from '@/contexts/GisContext/hook';
 
 const gisContext = createContext<GISContextType>({
   isSignedIn: false,
@@ -17,109 +9,11 @@ const gisContext = createContext<GISContextType>({
   handleSignOut: async () => {},
 });
 
-const clientId = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID || '';
-const scope = process.env.NEXT_PUBLIC_GOOGLE_SCOPE || '';
-
 export function GISProvider({ children }: PropsWithChildren) {
-  const { loadScript } = useLoadScript();
-  const { setGapiSettings, gapiStorage } = useStoragesStore();
-  const [tokenClient, setTokenClient] = useState<TokenClient | null>(null);
-
-  const isSignedIn = useMemo(() => (
-    Boolean(gapiStorage.token)
-  ), [gapiStorage.token]);
-
-  const createTokenClient = useCallback(() => {
-    if (!clientId || !scope) {
-      return;
-    }
-
-    setTokenClient((currentTokenClient) => {
-      if (currentTokenClient) {
-        return currentTokenClient;
-      }
-
-      return google.accounts.oauth2.initTokenClient({
-        client_id: clientId,
-        scope,
-        callback: (tokenResponse) => {
-          console.log('🤖 callback');
-          if (tokenResponse.error) {
-            throw new Error(tokenResponse.error_description);
-          }
-
-          setGapiSettings({
-            token: tokenResponse.access_token,
-            tokenExpiry: Date.now() + (parseInt(tokenResponse.expires_in || '0', 10) * 1000),
-          });
-        },
-      });
-    });
-  }, [setGapiSettings]);
-
-  useEffect(() => {
-    const { use, tokenExpiry, token } = gapiStorage;
-    if (!clientId || !use || !token) {
-      return;
-    }
-
-    const tokenExpiresInMs = (tokenExpiry || 0) - Date.now();
-
-    const tokenExpiryTimerHandle = window.setTimeout(() => {
-      if (!tokenClient) {
-        return;
-      }
-
-      console.log('🤖 Token expired!');
-      tokenClient.requestAccessToken({ prompt: '' });
-      setGapiSettings({
-        token: '',
-        tokenExpiry: 0,
-      });
-    }, tokenExpiresInMs);
-
-    return () => window.clearTimeout(tokenExpiryTimerHandle);
-  }, [gapiStorage, setGapiSettings, tokenClient]);
-
-  useEffect(() => {
-    if (!clientId || !gapiStorage.use) {
-      return;
-    }
-
-    const handle = setTimeout(async () => {
-      await loadScript('https://accounts.google.com/gsi/client');
-      await loadScript('https://apis.google.com/js/api.js');
-      await createTokenClient();
-    }, 1);
-
-    return () => clearTimeout(handle);
-  }, [gapiStorage.use, createTokenClient, loadScript]);
-
-  const handleSignIn = useCallback(async () => {
-    if (!tokenClient) {
-      console.log('no tokenClient');
-      return;
-    }
-
-    tokenClient.requestAccessToken({ prompt: '' });
-  }, [tokenClient]);
-
-  const handleSignOut = useCallback(async () => {
-    if (!gapiStorage.token) {
-      return;
-    }
-
-    google.accounts.oauth2.revoke(gapiStorage?.token, () => {
-      setGapiSettings({
-        token: '',
-        tokenExpiry: 0,
-      });
-    });
-  }, [gapiStorage.token, setGapiSettings]);
-
+  const contextValue = useContextHook();
 
   return (
-    <gisContext.Provider value={{ handleSignIn, handleSignOut, isSignedIn }}>
+    <gisContext.Provider value={contextValue}>
       {children}
     </gisContext.Provider>
   );
