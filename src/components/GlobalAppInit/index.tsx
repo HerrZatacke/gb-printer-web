dd6f3a59a6c76fd7ade78799482e595e89d07952
Andreas Hahn <zatacke@gmail.com>
2026-02-15 01:46:18 +0100

    Disabled debug code

--- src/components/GlobalAppInit/index.tsx
+++ src/components/GlobalAppInit/index.tsx
@@ -8,14 +8,14 @@ import { useStores } from '@/hooks/useStores';
 import useTrashbin from '@/hooks/useTrashbin';
 import { dropboxStorageTool } from '@/tools/dropboxStorage';
 
-if (typeof window !== 'undefined') {
-  import('@/tools/generateDebugImages')
-    .then(({ generateDebugImages }) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      window.generateDebugImages = generateDebugImages;
-    });
-}
+// if (typeof window !== 'undefined') {
+//   import('@/tools/generateDebugImages')
+//     .then(({ generateDebugImages }) => {
+//       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//       // @ts-expect-error
+//       window.generateDebugImages = generateDebugImages;
+//     });
+// }
 
 function GlobalAppInit({ children }: PropsWithChildren) {
   useFileDrop();
