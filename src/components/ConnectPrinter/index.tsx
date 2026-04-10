78dd5b83f8771e49a849eb0893bc079a308f6111
Andreas Hahn <zatacke@gmail.com>
2026-04-10 14:34:46 +0200

    Added option to trust and open a wifi printer through url parameter

--- src/components/ConnectPrinter/index.tsx
+++ src/components/ConnectPrinter/index.tsx
@@ -4,17 +4,27 @@ import {
   Stack,
   Typography,
 } from '@mui/material';
+import { useRouter, usePathname } from 'next/navigation';
 import { useTranslations } from 'next-intl';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import PrinterReport from '@/components/PrinterReport';
-import { useInteractionsStore, useSettingsStore } from '@/stores/stores';
+import { DialoqQuestionType } from '@/consts/dialog';
+import { useClientSearchParams } from '@/contexts/SearchParamsContext';
+import { useDialogsStore, useInteractionsStore, useSettingsStore } from '@/stores/stores';
+import cleanUrl from '@/tools/cleanUrl';
 
 function ConnectPrinter() {
   const t = useTranslations('ConnectPrinter');
-  const { printerUrls } = useSettingsStore();
+  const { printerUrls, setPrinterUrls } = useSettingsStore();
   const { setPrinterFunctions, setPrinterData } = useInteractionsStore();
+  const { dismissDialog, setDialog } = useDialogsStore();
   const printerWindow = useRef<Window | null>(null);
   const [openedUrl, setOpenedUrl] = useState('');
+  const { searchParams } = useClientSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const addPrinter = searchParams?.get('addPrinter') || null;
 
   useEffect(() => {
     const handle = window.setInterval(() => {
@@ -32,19 +42,64 @@ function ConnectPrinter() {
     };
   }, [setPrinterFunctions, setPrinterData]);
 
+  const openPrinter = useCallback((printerUrl: string) => {
+    printerWindow.current?.close();
+    const newWindow = window.open(`${printerUrl}remote.html`, 'remoteprinter', 'width=480,height=400');
+    printerWindow.current = newWindow;
+    setOpenedUrl(printerUrl);
+    setPrinterFunctions([]);
+    setPrinterData(null);
+  }, [setPrinterData, setPrinterFunctions]);
+
+  const closePrinter = useCallback(() => {
+    printerWindow.current?.close();
+    setOpenedUrl('');
+    setPrinterFunctions([]);
+    setPrinterData(null);
+  }, [setPrinterData, setPrinterFunctions]);
+
+  useEffect(() => {
+    const handle = window.setTimeout(() => {
+      if (addPrinter) {
+        const openPrinterUrl = cleanUrl(addPrinter, 'http');
+
+        if (printerUrls.includes(openPrinterUrl)) {
+          openPrinter(openPrinterUrl);
+          router.replace(pathname);
+        } else {
+          console.log(openPrinterUrl);
+          setDialog({
+            message: t('askAddPrinterTitle'),
+            questions: () => ([{
+              type: DialoqQuestionType.INFO,
+              label: t('askAddPrinterMessage', { openPrinterUrl }),
+              key: 'message',
+              severity: 'warning',
+            }]),
+            confirm: async () => {
+              dismissDialog(0);
+              router.replace(pathname);
+              setPrinterUrls([...printerUrls, openPrinterUrl]);
+              openPrinter(openPrinterUrl);
+            },
+            deny: async () => {
+              dismissDialog(0);
+              router.replace(pathname);
+            },
+          });
+        }
+      }
+    }, 1);
+
+    return () => window.clearTimeout(handle);
+  }, [addPrinter, dismissDialog, openPrinter, pathname, printerUrls, router, setDialog, setPrinterUrls, t]);
+
   return (
     <Stack direction="column" gap={2}>
       {printerUrls.map((printerUrl, index) => (
         openedUrl === printerUrl ? (
           <PrinterReport key={`${printerUrl}-report-${index}`}>
-            <Button
-              onClick={() => {
-                printerWindow.current?.close();
-                setOpenedUrl('');
-                setPrinterFunctions([]);
-                setPrinterData(null);
-              }}
-            >
+            <Button onClick={closePrinter}>
               {t('closePrinterPage')}
             </Button>
           </PrinterReport>
@@ -62,16 +117,7 @@ function ConnectPrinter() {
               variant="contained"
               fullWidth
             >
-              <Button
-                onClick={() => {
-                  printerWindow.current?.close();
-                  const newWindow = window.open(`${printerUrl}remote.html`, 'remoteprinter', 'width=480,height=400');
-                  printerWindow.current = newWindow;
-                  setOpenedUrl(printerUrl);
-                  setPrinterFunctions([]);
-                  setPrinterData(null);
-                }}
-              >
+              <Button onClick={() => openPrinter(printerUrl)}>
                 {t('openPrinterPage')}
               </Button>
             </ButtonGroup>
