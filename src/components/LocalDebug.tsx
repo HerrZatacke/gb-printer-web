c1eac329f3d46d27943bbe0f691105dfa70fb82a
Andreas Hahn <zatacke@gmail.com>
2026-07-19 17:37:24 +0200

    Disable debug output

--- src/components/LocalDebug.tsx
+++ src/components/LocalDebug.tsx
@@ -1,5 +1,6 @@
 import { Alert, Paper, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useImages } from '@/hooks/useImages';
 import { delay } from '@/tools/delay';
 
 function LocalDebug() {
@@ -12,6 +13,12 @@ function LocalDebug() {
       });
     }
   }, []);
+
+  // const { byAnyHashes: blueChannel } = useImages({ anyHashes: ['8ce194178ea421d56f06215c582dbec844566954'] }); // RGB-Bulli b-channel hash
+  // const { byAnyHashes: neutralChannel } = useImages({ anyHashes: ['5e20e1ee863a83e9d5a9c762391e58095e7b165e'] }); // RGB-Bulli n-channel hash
+
+  // const { byGroupId: rootGroupItems } = useImages({ groupId: '' });
+  // const { byGroupId: randomRgbGroupItems } = useImages({ groupId: '34ee68d9-16e4-46b8-a745-2a510950d858' });
 
   if (!shouldRender) {
     return null;
@@ -26,6 +33,12 @@ function LocalDebug() {
         <Alert severity="warning" variant="filled">
           Debug Stuff
         </Alert>
+        {/* <pre style={{ maxHeight: '30vh' }}> */}
+        {/*   {JSON.stringify(rootGroupItems, null, 2)} */}
+        {/* </pre> */}
+        {/* <pre style={{ maxHeight: '30vh' }}> */}
+        {/*   {JSON.stringify(randomRgbGroupItems, null, 2)} */}
+        {/* </pre> */}
       </Stack>
     </Paper>
   );
