a6c481f3977181b35fbfb0376898aa334220c9cc
Andreas Hahn <zatacke@gmail.com>
2026-07-25 13:28:13 +0200

    Use MUI buttons/groups for LocalDebug and apply "showDebug" toggle

--- src/components/LocalDebug.tsx
+++ src/components/LocalDebug.tsx
@@ -1,20 +1,28 @@
-import { Alert, Paper, Stack } from '@mui/material';
+import {
+  Alert,
+  Button,
+  ButtonGroup,
+  Paper,
+  Stack,
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useNavigationTools } from '@/contexts/NavigationToolsContext';
 // import { useImages } from '@/hooks/useImages';
 import useTrashbin from '@/hooks/useTrashbin';
+import { useSettingsStore } from '@/stores/stores';
 import { delay } from '@/tools/delay';
 
 function LocalDebug() {
   const [shouldRender, setShouldRender] = useState(false);
+  const { enableDebug } = useSettingsStore();
 
   useEffect(() => {
     if (window.location.hostname === 'localhost') {
       delay(1).then(() => {
-        setShouldRender(true);
+        setShouldRender(enableDebug);
       });
     }
-  }, []);
+  }, [enableDebug]);
 
   // const { images: allImages } = useImages({ list: true }); // All images
   // const { raw: rawImages1 } = useImages({ raw: { filters: { tags: ['testing'] }, sort: { field: 'created', direction: 'asc' }, page: 0, pageSize: 200 } });
@@ -47,58 +55,62 @@ function LocalDebug() {
         {/* <pre style={{ maxHeight: '30vh' }}>{rawImages1.length || 'no images'}</pre> */}
         {/* <pre style={{ maxHeight: '30vh' }}>{rawImages2.length || 'no images'}</pre> */}
         {/* <pre style={{ maxHeight: '30vh' }}>{JSON.stringify(rootGroupItems, null, 2)}</pre> */}
-        {/* <pre style={{ maxHeight: '30vh' }}>{JSON.stringify(randomRgbGroupItems, null, 2)}</pre> */}
-        <Stack direction="row">
-          <button onClick={async () => {
+        <ButtonGroup size="small" variant="contained" fullWidth color="secondary">
+          <Button onClick={async () => {
             await navigateToGroup('BadId', 0);
           }}>
             Bad Id/pageIndex 0
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToGroup('ROOT', 0);
           }}>
             ROOT/pageIndex 0
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToGroup('ROOT', 2);
           }}>
             ROOT/pageIndex 2
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToGroup('54e8e70e-68ea-400a-ae9d-0cee8e41f965', 0);
           }}>
             Drinks / pageIndex 0
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToGroup('54e8e70e-68ea-400a-ae9d-0cee8e41f965', 2);
           }}>
             Drinks / pageIndex 2
-          </button>
-          <hr />
-          <button onClick={async () => {
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup size="small" variant="contained" fullWidth color="secondary">
+          <Button onClick={async () => {
             await navigateToImage('BadHash');
           }}>
             To Image: Bad Hash
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToImage('75844f32d28fc7a47ce096b28aaf9114f7fa5582');
           }}>
             To Image: *Drinks 2* (Drinks / pageIndex:0)
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToImage('4d8c1dd7609c864834c24341990156da8c91a56a');
           }}>
             To Image: *Drinks 23* (Drinks / pageIndex:1)
-          </button>
-          <button onClick={async () => {
+          </Button>
+          <Button onClick={async () => {
             await navigateToImage('c89f0f97516aa784c0ccec1d0c9c1cf782b2fba5');
           }}>
             To Image: *Isar* (munich/isar / pageIndex:1)
-          </button>
-          <hr/>
-          <button onClick={checkUpdateTrashCount}>
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup size="small" variant="contained" fullWidth color="secondary">
+          <Button onClick={checkUpdateTrashCount}>
             checkUpdateTrashCount
-          </button>
+          </Button>
+        </ButtonGroup>
+        {/* <pre style={{ maxHeight: '30vh' }}>{JSON.stringify(randomRgbGroupItems, null, 2)}</pre> */}
+        <Stack direction="row">
         </Stack>
       </Stack>
     </Paper>
