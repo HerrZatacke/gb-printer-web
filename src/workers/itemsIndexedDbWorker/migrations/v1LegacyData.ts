728adb563d2158f4f63146b601ed450cb8216929
Andreas Hahn <zatacke@gmail.com>
2026-07-25 13:42:08 +0200

    Added try/catch safety for v1 legacy data migration when accessing old databases

--- src/workers/itemsIndexedDbWorker/migrations/v1LegacyData.ts
+++ src/workers/itemsIndexedDbWorker/migrations/v1LegacyData.ts
@@ -2,7 +2,7 @@ import {
   openDB,
   type IDBPDatabase,
   type StoreNames,
-  type StoreValue,
+  type StoreValue, IDBPTransaction,
 } from 'idb';
 import { type ZodSafeParseResult } from 'zod';
 import { FrameSchema } from '@/types/Frame';
@@ -40,12 +40,12 @@ export const v1LegacyData: AfterUpgradeFn = async (
     parser: (raw: unknown) => ZodSafeParseResult<StoreValue<ItemsDB, S>>,
     storeName: StoreNames<ItemsDB>,
   ) => {
-    const legacyTx = oldDb.transaction(oldStoreName, 'readonly');
-    const legacyStore = legacyTx.store;
-
     let oldData: unknown[];
     try {
+      const legacyTx = oldDb.transaction(oldStoreName, 'readonly');
+      const legacyStore = legacyTx.store;
       oldData = await legacyStore.getAll();
+      await legacyTx.done;
     } catch {
       olderLegacyData = olderLegacyData || await hostApi.getLegacyStorage(); // fallback to even older storage solution in localStorage
       oldData = olderLegacyData[olderLegacyKey] || [];
@@ -61,24 +61,30 @@ export const v1LegacyData: AfterUpgradeFn = async (
       await tx.store.put(result.data);
     }
 
-    await Promise.all([tx.done, legacyTx.done]);
+    await tx.done;
   };
 
   const updateFromLegacyBinaryData = async (
     oldStoreName: string,
     storeName: StoreNames<ItemsDB>,
   ) => {
-    const legacyTx = oldDb.transaction(oldStoreName, 'readonly');
-    const legacyStore = legacyTx.store;
-
-    let cursor = await legacyStore.openCursor();
     const oldData: { key: string; value: string }[] = [];
-    while (cursor) {
-      oldData.push({
-        key: cursor.key as string,
-        value: cursor.value,
-      });
-      cursor = await cursor.continue();
+    try {
+      const legacyTx = oldDb.transaction(oldStoreName, 'readonly');
+      const legacyStore = legacyTx.store;
+
+      let cursor = await legacyStore.openCursor();
+      while (cursor) {
+        oldData.push({
+          key: cursor.key as string,
+          value: cursor.value,
+        });
+        cursor = await cursor.continue();
+      }
+      await legacyTx.done;
+    } catch {
+      console.warn(`Did not copy binary data from ${oldStoreName}. Store probably does not exist.`);
+      return;
     }
 
     const tx = upgradedDatabase.transaction(storeName, 'readwrite');
@@ -95,7 +101,7 @@ export const v1LegacyData: AfterUpgradeFn = async (
       await tx.store.put(value, key as string);
     }
 
-    await Promise.all([tx.done, legacyTx.done]);
+    await tx.done;
   };
 
   try {
