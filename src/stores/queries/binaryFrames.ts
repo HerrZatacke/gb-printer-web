16d12331ffbe2ac46f0b1dc6171c1bacd6f057f1
Andreas Hahn <zatacke@gmail.com>
2026-07-25 12:34:04 +0200

    Added tanstack query factory for binary frames

---
+++ src/stores/queries/binaryFrames.ts
@@ -0,0 +1,15 @@
+import { getItemsSource } from '@/items/client';
+import { createBinaryBlobQueries } from '@/stores/queries/binaryStoreFactory';
+
+export const {
+  hashesQueryOptions: binaryFrameHashesQueryOptions,
+  byHashQueryOptions: binaryFrameByHashQueryOptions,
+  byHashesQueryOptions: binaryFramesByHashesQueryOptions,
+  updateAction: updateBinaryFramesAction,
+  deleteByHashesAction: deleteBinaryFramesByHashesAction,
+} = createBinaryBlobQueries('frames', {
+  getByHashes: async (hashes) => (await getItemsSource()).getBinaryFramesByHashes(hashes),
+  getHashes: async () => (await getItemsSource()).getBinaryFrameHashes(),
+  update: async (items) => (await getItemsSource()).updateBinaryFrames(items),
+  deleteByHashes: async (hashes) => (await getItemsSource()).deleteBinaryFramesByHashes(hashes),
+});
