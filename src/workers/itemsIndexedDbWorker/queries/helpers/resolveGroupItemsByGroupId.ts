0dcb8ca19af4e21d1d5582f97c0a878e43d88039
Andreas Hahn <zatacke@gmail.com>
2026-07-29 00:58:22 +0200

    Silenced "missing coverImages"/"missing images" warnings if filters are active

--- src/workers/itemsIndexedDbWorker/queries/helpers/resolveGroupItemsByGroupId.ts
+++ src/workers/itemsIndexedDbWorker/queries/helpers/resolveGroupItemsByGroupId.ts
@@ -80,30 +80,35 @@ export const resolveGroupItemsByGroupId = async (
   const coverImageHashes = filteredGroups.map((g) => g.coverImage);
   const groupImages = await resolveAndFilterImages(db, undefined, new Set(coverImageHashes));
 
-  if (coverImageHashes.length !== groupImages.length) {
-    const foundGroupImageHashes = new Set(groupImages.map(({ hash }) => hash));
-    const missingCovers = new Set(coverImageHashes.filter((coverHash) => !foundGroupImageHashes.has(coverHash)));
-    const badGroups = filteredGroups.filter(({ coverImage }) => missingCovers.has(coverImage));
-    console.warn(`missing coverimage(s) of childgroup(s) "${(imageGroup || rootGroup)?.title}" (${(imageGroup || rootGroup)?.id}):`, {
-      expectedLength: coverImageHashes.length,
-      foundLength: groupImages.length,
-      missingCovers,
-      badGroups,
-      foundGroupImageHashes,
-    });
+  const hasFilters = filters && Boolean(filters.tags?.length || filters.palette?.length || filters.frame?.length);
+
+  if (!hasFilters) {
+    if (coverImageHashes.length !== groupImages.length) {
+      const foundGroupImageHashes = new Set(groupImages.map(({ hash }) => hash));
+      const missingCovers = new Set(coverImageHashes.filter((coverHash) => !foundGroupImageHashes.has(coverHash)));
+      const badGroups = filteredGroups.filter(({ coverImage }) => missingCovers.has(coverImage));
+      console.warn(`missing coverimage(s) of childgroup(s) "${(imageGroup || rootGroup)?.title}" (${(imageGroup || rootGroup)?.id}):`, {
+        expectedLength: coverImageHashes.length,
+        foundLength: groupImages.length,
+        missingCovers,
+        badGroups,
+        foundGroupImageHashes,
+      });
+    }
+
+    if (imageHashes.length !== images.length) {
+      const foundImageHashes = new Set(images.map(({ hash }) => hash));
+      const missingImages = imageHashes.filter((coverHash) => !foundImageHashes.has(coverHash));
+      console.warn(`missing images from group "${(imageGroup || rootGroup)?.title}" (${(imageGroup || rootGroup)?.id}):`, {
+        expectedLength: imageHashes.length,
+        foundLength: images.length,
+        missingImages,
+        badGroup: imageGroup || rootGroup,
+        foundImageHashes,
+      });
+    }
   }
 
-  if (imageHashes.length !== images.length) {
-    const foundImageHashes = new Set(images.map(({ hash }) => hash));
-    const missingImages = imageHashes.filter((coverHash) => !foundImageHashes.has(coverHash));
-    console.warn(`missing images from group "${(imageGroup || rootGroup)?.title}" (${(imageGroup || rootGroup)?.id}):`, {
-      expectedLength: imageHashes.length,
-      foundLength: images.length,
-      missingImages,
-      badGroup: imageGroup || rootGroup,
-      foundImageHashes,
-    });
-  }
 
   const imageItems = images.map((image): GroupItem => {
     return {
