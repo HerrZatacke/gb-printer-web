835a0f1f995600bcd5cd774331377c024d4cd17d
Andreas Hahn <zatacke@gmail.com>
2026-08-16 18:43:00 +0200

    Prepared endpoint urls in debug form

--- packages/gb-items-db/src/routes/images.ts
+++ packages/gb-items-db/src/routes/images.ts
@@ -1,27 +1,50 @@
 import { type FastifyPluginAsync } from 'fastify';
-import { type GetImagesParams, type Image, type ItemsSourceResponse } from 'gb-printer-schemas';
+import {
+  type DeleteImagesByHashesParams,
+  type GetGroupItemsByGroupIdParams,
+  type GetHashesByGroupIdParams,
+  type GetImagesByAnyHashesParams,
+  type GetImagesByHashesParams,
+  type GetImagesParams,
+  type GroupItem,
+  type Image,
+  type ItemsReferenceList,
+  type ItemsSourceResponse,
+  type ItemsSourceTotalResponse,
+  type UpdateImagesParams,
+} from 'gb-printer-schemas';
 
 const imagesRoutes: FastifyPluginAsync = async (app) => {
   app.post('/images', async (request): Promise<ItemsSourceResponse<Image>> => {
+    return app.itemsSource.getImages(request.body as GetImagesParams);
+  });
 
-    try {
-      return app.itemsSource.getImages(request.body as GetImagesParams);
-    } catch (error) {
-      console.log(error);
-    }
+  app.post('/images/tags', async (): Promise<ItemsSourceTotalResponse<string>> => {
+    return app.itemsSource.getAllTags();
+  });
 
+  app.post('/images/byHashes', async (request): Promise<ItemsSourceResponse<Image>> => {
+    return app.itemsSource.getImagesByHashes(request.body as GetImagesByHashesParams);
+  });
 
-    return {
-      items: [],
-      paging: {
-        filtered: 0,
-        total: 0,
-        page: 0,
-        pageSize: 0,
-        maxPageIndex: 0,
-      },
-      duration: 12345,
-    };
+  app.post('/images/byAnyHashes', async (request): Promise<ItemsSourceResponse<ItemsReferenceList<Image>>> => {
+    return app.itemsSource.getImagesByAnyHashes(request.body as GetImagesByAnyHashesParams);
+  });
+
+  app.post('/images/hashesByGroupId', async (request): Promise<ItemsSourceTotalResponse<string>> => {
+    return app.itemsSource.getHashesByGroupId(request.body as GetHashesByGroupIdParams);
+  });
+
+  app.post('/images/groupItemsByGroupId', async (request): Promise<ItemsSourceResponse<GroupItem>> => {
+    return app.itemsSource.getGroupItemsByGroupId(request.body as GetGroupItemsByGroupIdParams);
+  });
+
+  app.post('/images/update', async (request): Promise<void> => {
+    return app.itemsSource.updateImages(request.body as UpdateImagesParams);
+  });
+
+  app.post('/images/delete', async (request): Promise<void> => {
+    return app.itemsSource.deleteImagesByHashes(request.body as DeleteImagesByHashesParams);
   });
 };
 
